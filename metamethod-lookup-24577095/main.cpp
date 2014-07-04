997594d5d6772c03f86af0c5a5ee50f8e179ac50
Kuba Ober <kuba@mareimbrium.org>
2014-07-04 16:43:01 -0400

    Add metamethod-lookup project.

---
+++ metamethod-lookup-24577095/main.cpp
@@ -0,0 +1,33 @@
+#include <QCoreApplication>
+#include <QMetaObject>
+#include <QMetaMethod>
+#include <QDebug>
+
+class MyObject : public QObject {
+   Q_OBJECT
+public:
+   Q_SLOT void aSlot() {}
+   Q_SLOT void aSlot2(int) {}
+   Q_SLOT int aSlot3(int) { return 0; }
+   Q_SIGNAL void aSignal();
+   Q_SIGNAL void aSignal2(int);
+};
+
+template <typename Func> int indexOfMethod(Func method)
+{
+   typedef QtPrivate::FunctionPointer<Func> FuncType;
+   int methodIndex = -1;
+   void *metaArgs[] = {&methodIndex, reinterpret_cast<void **>(&method)};
+   const QMetaObject & mo = FuncType::Object::staticMetaObject;
+   mo.static_metacall(QMetaObject::IndexOfMethod, 0, metaArgs);
+   return methodIndex;
+}
+
+int main(int argc, char *argv[])
+{
+   QCoreApplication a(argc, argv);
+   qDebug() << indexOfMethod(&MyObject::aSlot) << indexOfMethod(&MyObject::aSlot3) << indexOfMethod(&MyObject::aSignal2);
+   return 0;
+}
+
+#include "main.moc"
---
+++ metamethod-lookup-24577095/metamethod-lookup-24577095.pro
@@ -0,0 +1,18 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2014-07-04T16:12:08
+#
+#-------------------------------------------------
+
+QT       += core
+
+QT       -= gui
+
+TARGET = metamethod-lookup-24577095
+CONFIG   += console
+CONFIG   -= app_bundle
+
+TEMPLATE = app
+
+
+SOURCES += main.cpp

81a8d38b62619fa3e66368a4b8cee63a21eddbbf
Kuba Ober <kuba@mareimbrium.org>
2014-07-19 13:08:17 -0400

    More.

--- webelement-24824687/main.cpp
+++ webelement-24824687/main.cpp
@@ -1,8 +1,39 @@
-#include <QCoreApplication>
+#include <QApplication>
+#include <QWebPage>
+#include <QWebFrame>
+#include <QWebElement>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
-
-    return a.exec();
+    QApplication a(argc, argv);
+    const QString html(
+                "<html><head><title>Page Title</title>"
+                "<link rel=\"stylesheet\" type=\"text/css\" href=\"styles.css\">"
+                "<link rel=\"stylesheet\" type=\"text/css\" href=\"morestyles.css\">"
+                "<script type=\"text/javascript\" src=\"script1.php\"> </script>"
+                "<script type=\"text/javascript\" src=\"script2.php\"> </script>"
+                "<script type=\"text/javascript\" src=\"script3.php\"> </script>"
+                "<script type=\"text/javascript\" src=\"script4.php\"> </script>"
+                "</head><body>"
+                "<TABLE class=\"A\">"
+                "<TR class=\"A\">"
+                "<TD>some text...</TD>"
+                "<TD>some text...</TD>"
+                "</TR>"
+                "<TR class=\"A\">"
+                "<TD>some text...</TD>"
+                "<TD>some text...</TD>"
+                "</TR>"
+                "</TABLE>"
+                "</body></html>");
+    QWebPage p;
+    p.mainFrame()->setHtml(html);
+    QWebElement we(p.mainFrame()->documentElement());
+    QWebElementCollection elements(we.findAll(".A"));
+    QStringList elist;
+    foreach(const QWebElement &e, elements)
+        elist.append(e.toOuterXml());
+    qDebug() << elist;
+    return 0;
 }
--- webelement-24824687/webelement-24824687.pro
+++ webelement-24824687/webelement-24824687.pro
@@ -1,18 +1,7 @@
-#-------------------------------------------------
-#
-# Project created by QtCreator 2014-07-18T08:17:13
-#
-#-------------------------------------------------
-
-QT       += core
-
-QT       -= gui
+QT       += webkitwidgets
 
 TARGET = webelement-24824687
-CONFIG   += console
-CONFIG   -= app_bundle
 
 TEMPLATE = app
-
 
 SOURCES += main.cpp

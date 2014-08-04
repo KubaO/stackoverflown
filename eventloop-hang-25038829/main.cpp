f796132d4c2802b598229fc523002649af99159f
Kuba Ober <kuba@mareimbrium.org>
2014-08-04 07:16:16 -0400

    wip

--- eventloop-hang-25038829/main.cpp
+++ eventloop-hang-25038829/main.cpp
@@ -1,5 +1,8 @@
 #include <QApplication>
-#include <QWidget>
+#include <QLabel>
+#include <QGridLayout>
+#include <QTableView>
+#include <QGroupBox>
 #include <QThread>
 #include <QDateTime>
 #include <QElapsedTimer>
@@ -7,10 +10,10 @@
 #include <QMutex>
 #include <QMap>
 #include <QStandardItemModel>
-#include <QTableView>
 #include <QtConcurrent>
 #include <QPainter>
-#include <QDebug>
+#include <QSlider>
+#include <random>
 
 int ilog2(qint64 val) {
    Q_ASSERT(val >= 0);
@@ -38,6 +41,7 @@ public:
    ~Thread() { quit(); wait(); }
 };
 
+/// An application that monitors event loops in all threads.
 class LoopMonitoringApp : public QApplication {
    Q_OBJECT
 public:
@@ -51,9 +55,11 @@ private:
       /// was entered, or zero if no event handler is running.
       qint64 ping;
       /// Number of milliseconds between the epoch and when the last histogram update for
-      /// this thread was broadcast;
+      /// this thread was broadcast
       qint64 update;
-      ThreadData() : ping(0), update(0) {}
+      /// Whether the thread's event loop is considered stuck at the moment
+      bool stuck;
+      ThreadData() : ping(0), update(0), stuck(false) {}
    };
    typedef QMap<QThread*, ThreadData> Threads;
    QMutex m_mutex;
@@ -67,15 +73,22 @@ private:
       void timerEvent(QTimerEvent * ev) Q_DECL_OVERRIDE {
          if (ev->timerId() != m_timer.timerId()) return;
          int timeout = m_app->m_timeout;
-         QMutexLocker lock(&m_app->m_mutex);
-         const Threads threads(m_app->m_threads);
-         lock.unlock();
          qint64 now = QDateTime::currentMSecsSinceEpoch();
-         for (auto it = threads.constBegin(); it != threads.constEnd(); ++it) {
+         QList<QPair<QThread*, int>> toEmit;
+         QMutexLocker lock(&m_app->m_mutex);
+         for (auto it = m_app->m_threads.begin(); it != m_app->m_threads.end(); ++it) {
             if (it->ping == 0) continue;
             qint64 elapsed = now - it->ping;
-            if (elapsed > timeout) emit m_app->stuckEventLoop(it.key(), elapsed);
+            if (elapsed > timeout) {
+               it->stuck = true;
+               toEmit << qMakePair(it.key(), elapsed);
+            } else {
+               if (it->stuck) toEmit << qMakePair(it.key(), 0);
+               it->stuck = false;
+            }
          }
+         lock.unlock();
+         for (auto & sig : toEmit) emit m_app->loopStateChanged(sig.first, sig.second);
       }
    public:
       explicit StuckEventLoopNotifier(LoopMonitoringApp * app) : m_app(app) {
@@ -90,9 +103,11 @@ private:
       typename Threads::iterator it = m_threads.find(thread);
       if (it == m_threads.end()) return;
       Histogram const histogram(it->histogram);
+      bool stuck = it->stuck;
       m_threads.erase(it);
       lock.unlock();
       emit newHistogram(thread, histogram);
+      if (stuck) emit loopStateChanged(thread, 0);
       emit threadFinished(thread);
    }
 protected:
@@ -103,7 +118,6 @@ protected:
       QMutexLocker lock(&m_mutex);
       typename Threads::iterator it = m_threads.find(curThread);
       bool newThread = it == m_threads.end();
-      if (newThread) qDebug() << "new thread" << curThread;
       if (newThread) it = m_threads.insert(curThread, ThreadData());
       it->ping = now;
       lock.unlock();
@@ -114,8 +128,7 @@ protected:
                                    Q_ARG(QString, curThread->objectName()));
       }
       timer.start();
-      // This is where the event loop can get "stuck".
-      bool result = Base::notify(receiver, event);
+      bool result = Base::notify(receiver, event); // This is where the event loop can get "stuck".
       qint64 duration = binned(timer.elapsed(), 3);
       now += duration;
       lock.relock();
@@ -134,9 +147,11 @@ protected:
    }
 public:
    explicit LoopMonitoringApp(int & argc, char ** argv);
-   /// The event loop for given thread is stuck.
-   /** The thread might not exist when this notification is received. */
-   Q_SIGNAL void stuckEventLoop(QThread *, int elapsed);
+   /// The event loop for a given thread has gotten stuck, or unstuck.
+   /** A zero elapsed time indicates that the loop is not stuck. The signal will be
+    * emitted periodically with increasing values of `elapsed` for a given thread as long
+    * as the loop is stuck. The thread might not exist when this notification is received. */
+   Q_SIGNAL void loopStateChanged(QThread *, int elapsed);
    /// The first event was received in a newly started thread's event loop.
    /** The thread might not exist when this notification is received. */
    Q_SIGNAL void newThread(QThread *, const QString & threadName);
@@ -169,50 +184,58 @@ LoopMonitoringApp::LoopMonitoringApp(int & argc, char ** argv) :
 }
 
 QImage renderHistogram(const LoopMonitoringApp::Histogram & h) {
-   QImage img(1+h.size(), 32*2, QImage::Format_ARGB32_Premultiplied);
+   const int blockX = 2, blockY = 2;
+   QImage img(1 + h.size() * blockX, 32 * blockY, QImage::Format_ARGB32_Premultiplied);
    img.fill(Qt::white);
    QPainter p(&img);
    int x = 0;
    for (LoopMonitoringApp::Histogram::const_iterator it = h.begin(); it != h.end(); ++it) {
       qreal key = it.key() > 0 ? log2(it.key()) : 0.0;
-      p.setPen(QColor::fromHsv(qRound(240.0*(1.0 - key/32.0)), 255, 255));
-      p.drawLine(QPointF(x, 0), QPointF(x, log2(it.value())*2.0));
-      ++ x;
+      QBrush b = QColor::fromHsv(qRound(240.0*(1.0 - key/32.0)), 255, 255);
+      p.fillRect(QRectF(x, img.height(), blockX, -log2(it.value()) * blockY), b);
+      x += blockX;
    }
    return img;
 }
 
 class LoopMonitoringViewModel : public QStandardItemModel {
    Q_OBJECT
-   typedef QMap<QThread*, QStandardItem*> ThreadItems;
+   typedef QMap<QThread*, QPair<QStandardItem*, QStandardItem*>> ThreadItems;
    ThreadItems m_threadItems;
    Q_SLOT void newThread(QThread * thread, const QString & threadName) {
-      qDebug() << thread << threadName;
-      QStandardItem * histogram;
+      QString const caption = QString("0x%1 \"%2\"").arg(std::intptr_t(thread), 0, 16).arg(threadName);
       int row = rowCount() ? 1 : 0;
       insertRow(row);
-      setItem(row, 0, new QStandardItem(QString("0x%1 \"%2\"").arg(std::intptr_t(thread), 0, 16).arg(threadName)));
-      setItem(row, 1, histogram = new QStandardItem());
-      m_threadItems[thread] = histogram;
-      newHistogram(thread, LoopMonitoringApp::Histogram());
+      QStandardItem * captionItem = new QStandardItem(caption);
+      captionItem->setEditable(false);
+      setItem(row, 0, captionItem);
+      QStandardItem * histogram = new QStandardItem;
+      histogram->setEditable(false);
+      setItem(row, 1, histogram);
+      m_threadItems[thread] = qMakePair(captionItem, histogram);
+       newHistogram(thread, LoopMonitoringApp::Histogram());
    }
    Q_SLOT void newHistogramImage(QThread * thread, const QImage & img) {
       ThreadItems::iterator it = m_threadItems.find(thread);
       if (it == m_threadItems.end()) return;
-      (*it)->setSizeHint(img.size());
-      (*it)->setData(img, Qt::DecorationRole);
+      it->second->setSizeHint(img.size());
+      it->second->setData(img, Qt::DecorationRole);
    }
    Q_SIGNAL void newHistogramImageSignal(QThread * thread, const QImage & img);
    Q_SLOT void newHistogram(QThread * thread, const LoopMonitoringApp::Histogram & histogram) {
       QtConcurrent::run([this, thread, histogram]{
-         emit newHistogramImage(thread, renderHistogram(histogram));
+         emit newHistogramImageSignal(thread, renderHistogram(histogram));
       });
+   }
+   Q_SLOT void loopStateChanged(QThread * thread, int elapsed) {
+      ThreadItems::iterator it = m_threadItems.find(thread);
+      if (it == m_threadItems.end()) return;
+      it->first->setData(elapsed ? QColor(Qt::red) : QColor(Qt::transparent), Qt::BackgroundColorRole);
    }
    Q_SLOT void threadFinished(QThread * thread) {
       ThreadItems::iterator it = m_threadItems.find(thread);
       if (it == m_threadItems.end()) return;
-      QStandardItem * caption = (*it)->parent()->child((*it)->row(), 0);
-      caption->setText(QString("Finished %1").arg(caption->text()));
+      it->first->setText(QString("Finished %1").arg(it->first->text()));
       m_threadItems.remove(thread);
    }
 public:
@@ -224,16 +247,57 @@ public:
       connect(app, SIGNAL(newHistogram(QThread*,LoopMonitoringApp::Histogram)),
               SLOT(newHistogram(QThread*,LoopMonitoringApp::Histogram)));
       connect(app, SIGNAL(threadFinished(QThread*)), SLOT(threadFinished(QThread*)));
+      connect(app, SIGNAL(loopStateChanged(QThread*,int)), SLOT(loopStateChanged(QThread*,int)));
    }
+};
+
+class WorkerObject : public QObject {
+   Q_OBJECT
+   QBasicTimer m_timer;
+   void timerEvent(QTimerEvent * ev) {
+      if (ev->timerId() != m_timer.timerId()) return;
+      QThread::msleep(1100);
+   }
+public:
+   WorkerObject(QObject * parent = 0) : QObject(parent) {}
+   Q_SLOT void start() { m_timer.start(0, this); }
 };
 
 int main(int argc, char *argv[])
 {
    LoopMonitoringApp app(argc, argv);
    LoopMonitoringViewModel model;
+   WorkerObject workerObject;
+   Thread workerThread;
+
+   QWidget w;
+   QGridLayout layout(&w);
    QTableView view;
+   QLabel timeoutLabel;
+   QSlider timeout(Qt::Horizontal);
+   QGroupBox worker("Worker Thread");
+   worker.setCheckable(true);
+   worker.setChecked(false);
+   QGridLayout wLayout(&worker);
+
    view.setModel(&model);
-   view.show();
+   layout.addWidget(&view, 0, 0, 1, 3);
+   timeout.setMinimum(50);
+   timeout.setMaximum(5000);
+   layout.addWidget(new QLabel("Timeout"), 1, 0);
+   layout.addWidget(&timeoutLabel, 1, 1);
+   layout.addWidget(&timeout, 1, 2);
+   timeoutLabel.connect(&timeout, SIGNAL(valueChanged(int)), SLOT(setNum(int)));
+   layout.addWidget(&worker, 2, 0, 1, 1);
+
+   QObject::connect(&worker, &QGroupBox::toggled, [&](bool run) {
+      if (run) {
+         workerThread.start();
+         QMetaObject::invokeMethod(&workerObject, "start");
+      } else workerThread.quit();
+   });
+   workerObject.moveToThread(&workerThread);
+   w.show();
    return app.exec();
 }
 

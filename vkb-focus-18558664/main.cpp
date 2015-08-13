939c5bf47c82d5b81678a528cc3ee8ce96a00893
Kuba Ober <kuba@mareimbrium.org>
2015-08-13 12:16:08 -0400

    Tweak.

--- vkb-focus-18558664/main.cpp
+++ vkb-focus-18558664/main.cpp
@@ -8,60 +8,54 @@
 #include <QDebug>
 
 class Keyboard : public QDockWidget {
-    Q_OBJECT
-    void init() {
-        auto * w = new QWidget;
-        auto * l = new QGridLayout(w);
-        for (int i = 0; i < 10; ++ i) {
-            auto * btn = new QToolButton;
-            btn->setText(QString::number(i));
-            btn->setProperty("key", Qt::Key_0 + i);
-            l->addWidget(btn, 0, i, 1, 1);
-            connect(btn, SIGNAL(clicked()), SLOT(clicked()));
-        }
-        setWidget(w);
-        setFeatures(QDockWidget::DockWidgetFloatable | QDockWidget::DockWidgetMovable);
-        foreach(QObject *o, widget()->children()) {
-            if (o->isWidgetType()) static_cast<QWidget*>(o)->setFocusPolicy(Qt::NoFocus);
-        }
-    }
-    void sendKey(Qt::Key key, Qt::KeyboardModifier mod)
-    {
-        if (! parentWidget()) return;
-        QWidget * target = parentWidget()->focusWidget();
-        if (! target) return;
+   Q_OBJECT
+   void init() {
+      auto w = new QWidget;
+      auto l = new QGridLayout(w);
+      for (int i = 0; i < 10; ++ i) {
+         auto btn = new QToolButton;
+         btn->setText(QString::number(i));
+         btn->setProperty("key", Qt::Key_0 + i);
+         l->addWidget(btn, 0, i, 1, 1);
+         connect(btn, SIGNAL(clicked()), SLOT(clicked()));
+      }
+      setWidget(w);
+      setFeatures(QDockWidget::DockWidgetFloatable | QDockWidget::DockWidgetMovable);
+      for (auto o : widget()->children()) {
+         if (o->isWidgetType()) static_cast<QWidget*>(o)->setFocusPolicy(Qt::NoFocus);
+      }
+   }
+   void sendKey(Qt::Key key, Qt::KeyboardModifier mod)
+   {
+      if (! parentWidget()) return;
+      auto target = parentWidget()->focusWidget();
+      if (! target) return;
 
-        QString repr = QKeySequence(key).toString();
-        QKeyEvent *pressEvent = new QKeyEvent(QEvent::KeyPress, key, mod, repr);
-        QKeyEvent *releaseEvent = new QKeyEvent(QEvent::KeyRelease, key, mod, repr);
-        qApp->postEvent(target, pressEvent);
-        qApp->postEvent(target, releaseEvent);
-        qDebug() << repr;
-    }
-    Q_SLOT void clicked() {
-        QVariant key = sender()->property("key");
-        if (key.isValid()) sendKey((Qt::Key)key.toInt(), Qt::NoModifier);
-    }
-
+      auto repr = QKeySequence(key).toString();
+      auto pressEvent = new QKeyEvent(QEvent::KeyPress, key, mod, repr);
+      auto releaseEvent = new QKeyEvent(QEvent::KeyRelease, key, mod, repr);
+      qApp->postEvent(target, pressEvent);
+      qApp->postEvent(target, releaseEvent);
+      qDebug() << repr;
+   }
+   Q_SLOT void clicked() {
+      auto key = sender()->property("key");
+      if (key.isValid()) sendKey((Qt::Key)key.toInt(), Qt::NoModifier);
+   }
 public:
-    explicit Keyboard(const QString & title, QWidget *parent = 0) : QDockWidget(title, parent) { init(); }
-    explicit Keyboard(QWidget *parent = 0) : QDockWidget(parent) { init(); }
+   Keyboard(const QString & title, QWidget *parent = 0) :
+      QDockWidget(title, parent) { init(); }
+   explicit Keyboard(QWidget *parent = 0) : QDockWidget(parent) { init(); }
 };
 
-class Window : public QMainWindow {
-public:
-    Window(QWidget *parent = 0, Qt::WindowFlags f = 0) : QMainWindow(parent, f) {
-        setCentralWidget(new QLineEdit);
-    }
-};
-
-int main(int argc, char *argv[])
+int main(int argc, char ** argv)
 {
-    QApplication a(argc, argv);
-    Window w;
-    w.addDockWidget(Qt::TopDockWidgetArea, new Keyboard("Keyboard", &w));
-    w.show();
-    return a.exec();
+   QApplication a(argc, argv);
+   QMainWindow w;
+   w.setCentralWidget(new QLineEdit);
+   w.addDockWidget(Qt::TopDockWidgetArea, new Keyboard("Keyboard", &w));
+   w.show();
+   return a.exec();
 }
 
 #include "main.moc"
--- vkb-focus-18558664/vkb-focus-18558664.pro
+++ vkb-focus-18558664/vkb-focus-18558664.pro
@@ -1,17 +1,6 @@
-#-------------------------------------------------
-#
-# Project created by QtCreator 2013-09-02T11:53:41
-#
-#-------------------------------------------------
-
-QT       += core gui
-
+QT       += gui
 greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
-
+CONFIG += c++11
 TARGET = vkb-focus-18558664
 TEMPLATE = app
-
-
 SOURCES += main.cpp
-
-HEADERS  +=

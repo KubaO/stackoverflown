d1ce7a7aa20f19aaf2cd1f76ad2574f2ff164139
Kuba Ober <kuba@mareimbrium.org>
2015-09-06 14:00:44 -0400

    More

---
+++ questions/graphics-resizable-32416527/graphics-resizable-32416527.pro
@@ -0,0 +1,6 @@
+QT = widgets 
+CONFIG += c++11
+TARGET = graphics-resizable-32416527
+TEMPLATE = app
+SOURCES += main.cpp
+
---
+++ questions/graphics-resizable-32416527/main.cpp
@@ -0,0 +1,8 @@
+// https://github.com/KubaO/stackoverflown/tree/master/questions/graphics-resizable-32416527
+#include <QtWidgets>
+
+int main(int argc, char ** argv) {
+    QApplication app{argc, argv};
+    return app.exec();
+}
+

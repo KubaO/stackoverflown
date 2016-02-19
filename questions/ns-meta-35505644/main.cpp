b952e2ab14bb2a6d77056333e86f667f340735d5
Kuba Ober <kuba@mareimbrium.org>
2016-02-19 10:31:48 -0500

    More.

---
+++ questions/ns-meta-35505644/main.cpp
@@ -0,0 +1,35 @@
+// https://github.com/KubaO/stackoverflown/tree/master/questions/ns-meta-35505644
+#include <QtCore>
+
+int A_a, B_a;
+
+class A : public QObject {
+   Q_OBJECT
+public:
+   Q_INVOKABLE A(int a, QObject * parent = 0) : QObject{parent} {
+      A_a = a;
+   }
+};
+
+namespace NS {
+class B : public QObject {
+   Q_OBJECT
+public:
+   Q_INVOKABLE B(int a, QObject * parent = 0) : QObject{parent} {
+      B_a = a;
+   }
+};
+}
+
+int main() {
+   Q_ASSERT(A_a == 0);
+   Q_ASSERT(B_a == 0);
+   QScopedPointer<QObject> a {A::staticMetaObject.newInstance(Q_ARG(int, 10))};
+   Q_ASSERT(A_a == 10);
+   QScopedPointer<QObject> b {NS::B::staticMetaObject.newInstance(Q_ARG(int, 20))};
+   Q_ASSERT(B_a == 20);
+   QScopedPointer<QObject> c {b->metaObject()->newInstance(Q_ARG(int, 30))};
+   Q_ASSERT(B_a == 30);
+}
+
+#include "main.moc"
---
+++ questions/ns-meta-35505644/ns-meta-35505644.pro
@@ -0,0 +1,6 @@
+QT = core
+CONFIG += c++11
+TARGET = ns-meta-35505644
+TEMPLATE = app
+SOURCES += main.cpp
+

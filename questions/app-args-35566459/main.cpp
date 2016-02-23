db3e4c64e1a19b26867c2a47597d5a17ef7bb18c
Kuba Ober <kuba@mareimbrium.org>
2016-02-23 11:43:45 -0500

    Tweak.

--- questions/app-args-35566459/main.cpp
+++ questions/app-args-35566459/main.cpp
@@ -1,12 +1,13 @@
 // https://github.com/KubaO/stackoverflown/tree/master/questions/app-args-35566459
 #include <QtCore>
+#include <memory>
+
+std::unique_ptr<QCoreApplication> newApp(int & argc, char ** & argv) {
+   return std::unique_ptr<QCoreApplication>(new QCoreApplication{argc, argv});
+}
 
 int main(int argc, char ** argv) {
-    QScopedPointer<QCoreApplication> app;
-    {
-        auto & tmp_argc = argc;
-        app.reset(new QCoreApplication{tmp_argc, argv});
-    }
+    std::unique_ptr<QCoreApplication> app(newApp(argc, argv));
     QSettings settings{"testvendor"};
     qDebug() << "Num of arguments: " << app->arguments().count();
 }

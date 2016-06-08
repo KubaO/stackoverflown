4ca009797875d07e5f3515469ff339c7ad431b56
Kuba Ober <kuba@mareimbrium.org>
2016-06-08 16:23:22 -0400

    More.

---
+++ questions/scene-print-37708423/main.cpp
@@ -0,0 +1,49 @@
+// https://github.com/KubaO/stackoverflown/tree/master/questions/scene-print-37708423
+#include <QtWidgets>
+#include <QtPrintSupport>
+
+int main(int argc, char *argv[])
+{
+   QApplication app(argc, argv);
+   QGraphicsScene scene;
+   QGraphicsView view(&scene);
+
+   auto in = 72.0f;
+   QRectF canvasRect(0, 0, 4*in, 4*in);
+   // this is to show actual scene
+   QGraphicsRectItem sss(canvasRect);
+   sss.setBrush(Qt::blue);
+   scene.addItem(&sss);
+   // this item is partially outside top left
+   QGraphicsEllipseItem e1(-0.5*in, -0.5*in, 1*in, 1*in);
+   e1.setBrush(Qt::yellow);
+   scene.addItem(&e1);
+   // this item is partially outside center
+   QGraphicsEllipseItem e2(2*in, 2*in, 2.5*in, 1*in);
+   e2.setBrush(Qt::yellow);
+   scene.addItem(&e2);
+   // this item is partially outside right
+   QGraphicsEllipseItem e3(3.5*in, 3.5*in, 1*in, 1*in);
+   e3.setBrush(Qt::yellow);
+   scene.addItem(&e3);
+
+   view.fitInView(scene.sceneRect());
+   view.show();
+
+   QPrinter printer;
+   QPrintDialog printDialog(&printer);
+   QObject::connect(&printDialog, &QDialog::accepted, [&]{
+      printer.setOrientation(QPrinter::Landscape);
+      QPainter painter(&printer);
+
+      auto source = canvasRect;
+      auto scale = printer.resolution()/in;
+      auto page = printer.pageRect(QPrinter::DevicePixel);
+      auto target = QRectF(page.topLeft(), source.size()*scale);
+      target &= page; // clip target rect to page
+      qDebug() << page << scale << source << target;
+      scene.render(&painter, target, source);
+   });
+   printDialog.show(); // modal on OS X thus must succeed `connect` above
+   return app.exec();
+}
---
+++ questions/scene-print-37708423/scene-print-37708423.pro
@@ -0,0 +1,5 @@
+QT = widgets printsupport
+CONFIG += c++11
+TARGET = scene-print-37708423
+TEMPLATE = app
+SOURCES += main.cpp

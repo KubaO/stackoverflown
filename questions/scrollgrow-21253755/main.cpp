7db8e8300323404d6d18cfa3aa6ff0c0800b1d2c
Kuba Ober <kuba@mareimbrium.org>
2016-06-22 10:18:50 -0400

    Fix a bug in resize event filtering. Use uniform initialization and eschew dynamic allocations.

--- questions/scrollgrow-21253755/main.cpp
+++ questions/scrollgrow-21253755/main.cpp
@@ -1,80 +1,74 @@
-#include <QApplication>
-#include <QScrollArea>
-#include <QLabel>
-#include <QPushButton>
-#include <QBoxLayout>
-#include <QGridLayout>
-
-class XYZ {
-public:
-    operator QObject*() { return 0; }
-};
+// https://github.com/KubaO/stackoverflown/tree/master/questions/scrollgrow-21253755
+#include <QtGui>
+#if QT_VERSION >= QT_VERSION_CHECK(5,0,0)
+#include <QtWidgets>
+#endif
 
 class ButtonGroup : public QWidget {
-    Q_OBJECT
+   Q_OBJECT
+   QHBoxLayout m_layout{this};
 public:
-    ButtonGroup(QWidget * parent = 0) : QWidget(parent) {
-        new QHBoxLayout(this);
-        layout()->setSizeConstraint(QLayout::SetMinAndMaxSize); // <<< Essential
-    }
-    Q_SLOT void addButton() {
-        int n = layout()->count();
-        layout()->addWidget(new QPushButton(QString("Btn #%1").arg(n+1)));
-    }
+   ButtonGroup(QWidget * parent = 0) : QWidget{parent} {
+      m_layout.setSizeConstraint(QLayout::SetMinAndMaxSize); // <<< Essential
+   }
+   Q_SLOT void addButton() {
+      auto n = m_layout.count();
+      m_layout.addWidget(new QPushButton{QString{"Btn #%1"}.arg(n+1)});
+   }
 };
 
 class AdjustingScrollArea : public QScrollArea {
-    bool eventFilter(QObject * obj, QEvent * ev) {
-        if (obj == widget() && ev->type() != QEvent::Resize) {
-            // Essential vvv
-            setMaximumWidth(width() - viewport()->width() + widget()->width());
-        }
-        return QScrollArea::eventFilter(obj, ev);
-    }
+   bool eventFilter(QObject * obj, QEvent * ev) {
+      if (obj == widget() && ev->type() == QEvent::Resize) {
+         // Essential vvv
+         setMaximumWidth(width() - viewport()->width() + widget()->width());
+      }
+      return QScrollArea::eventFilter(obj, ev);
+   }
 public:
-    AdjustingScrollArea(QWidget * parent = 0) : QScrollArea(parent) {}
-    void setWidget(QWidget *w) {
-        QScrollArea::setWidget(w);
-        // It so happens that QScrollArea already filters widget events,
-        // but that's an implementation detail that we shouldn't rely on.
-        w->installEventFilter(this);
-    }
+   AdjustingScrollArea(QWidget * parent = 0) : QScrollArea{parent} {}
+   void setWidget(QWidget *w) {
+      QScrollArea::setWidget(w);
+      // It happens that QScrollArea already filters widget events,
+      // but that's an implementation detail that we shouldn't rely on.
+      w->installEventFilter(this);
+   }
 };
 
 class Window : public QWidget {
-    Q_OBJECT
+   QGridLayout         m_layout{this};
+   QLabel              m_left{">>"};
+   AdjustingScrollArea m_area;
+   QLabel              m_right{"<<"};
+   QPushButton         m_add{"Add a widget"};
+   ButtonGroup         m_group;
 public:
-    Window() {
-        QGridLayout * layout = new QGridLayout(this);
-        QWidget * w;
-        QScrollArea * area;
-        ButtonGroup * group;
+   Window() {
+      m_layout.addWidget(&m_left, 0, 0);
+      m_left.setSizePolicy(QSizePolicy::Maximum, QSizePolicy::Preferred);
+      m_left.setStyleSheet("border: 1px solid green");
 
-        layout->addWidget(w = new QLabel(">>"), 0, 0);
-        w->setSizePolicy(QSizePolicy::Maximum, QSizePolicy::Preferred);
-        w->setStyleSheet("border: 1px solid green");
+      m_layout.addWidget(&m_area, 0, 1);
+      m_area.setSizePolicy(QSizePolicy::MinimumExpanding, QSizePolicy::Preferred);
+      m_area.setStyleSheet("QScrollArea { border: 1px solid blue }");
+      m_area.setWidget(&m_group);
+      m_layout.setColumnStretch(1, 1);
 
-        layout->addWidget(area = new AdjustingScrollArea, 0, 1);
-        area->setSizePolicy(QSizePolicy::MinimumExpanding, QSizePolicy::Preferred);
-        area->setStyleSheet("QScrollArea { border: 1px solid blue }");
-        area->setWidget(group = new ButtonGroup);
-        layout->setColumnStretch(1, 1);
+      m_layout.addWidget(&m_right, 0, 2);
+      m_right.setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Preferred);
+      m_right.setStyleSheet("border: 1px solid green");
 
-        layout->addWidget(w = new QLabel("<<"), 0, 2);
-        w->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Preferred);
-        w->setStyleSheet("border: 1px solid green");
-
-        layout->addWidget(w = new QPushButton("Add a widget"), 1, 0, 1, 3);
-        connect(w, SIGNAL(clicked()), group, SLOT(addButton()));
-    }
+      m_layout.addWidget(&m_add, 1, 0, 1, 3);
+      connect(&m_add, SIGNAL(clicked()), &m_group, SLOT(addButton()));
+   }
 };
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    Window w;
-    w.show();
-    return a.exec();
+   QApplication a{argc, argv};
+   Window w;
+   w.show();
+   return a.exec();
 }
 
 #include "main.moc"
--- questions/scrollgrow-21253755/scrollgrow-21253755.pro
+++ questions/scrollgrow-21253755/scrollgrow-21253755.pro
@@ -1,8 +1,14 @@
-QT       += core gui
-greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
-
+greaterThan(QT_MAJOR_VERSION, 4) {
+    QT = widgets
+    CONFIG += c++11
+} else {
+    QT = gui
+    unix:QMAKE_CXXFLAGS += -std=c++11
+    macx {
+        QMAKE_CXXFLAGS += -stdlib=libc++
+        QMAKE_MACOSX_DEPLOYMENT_TARGET = 10.7
+    }
+}
 TARGET = scrollgrow-21253755
-
 TEMPLATE = app
-
 SOURCES += main.cpp

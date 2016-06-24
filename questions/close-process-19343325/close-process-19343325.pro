01a36853eaba8c036353d1ce8bd23a16005c84ac
Kuba Ober <kuba@mareimbrium.org>
2016-06-24 12:32:23 -0400

    Qt 4/5 variant

--- questions/close-process-19343325/close-process-19343325.pro
+++ questions/close-process-19343325/close-process-19343325.pro
@@ -1,14 +1,14 @@
-#-------------------------------------------------
-#
-# Project created by QtCreator 2013-10-13T11:19:49
-#
-#-------------------------------------------------
-
-QT       += core gui widgets
-
+greaterThan(QT_MAJOR_VERSION, 4) {
+    QT = widgets
+    CONFIG += c++11
+} else {
+    QT = gui
+    unix:QMAKE_CXXFLAGS += -std=c++11
+    macx {
+        QMAKE_CXXFLAGS += -stdlib=libc++
+        QMAKE_MACOSX_DEPLOYMENT_TARGET = 10.7
+    }
+}
 TARGET = close-process-19343325
-
 TEMPLATE = app
-
-
 SOURCES += main.cpp
--- questions/close-process-19343325/main.cpp
+++ questions/close-process-19343325/main.cpp
@@ -6,15 +6,14 @@
 class Object : public QObject {
     Q_OBJECT
     QBasicTimer m_timer;
-    int m_counter;
+    int m_counter = 0;
 protected:
     void timerEvent(QTimerEvent * ev) {
-        if (ev->timerId() == m_timer.timerId()) {
+        if (ev->timerId() == m_timer.timerId())
             m_counter ++;
-        }
     }
 public:
-    Object(QObject * parent = 0) : QObject(parent), m_counter(0) {
+    Object(QObject * parent = 0) : QObject{parent} {
         m_timer.start(0, this);
     }
     Q_SLOT void notify() const { emit countedTo(m_counter); }
@@ -23,25 +22,25 @@ public:
 
 int main(int argc, char** argv)
 {
-    QApplication app(argc, argv);
+    QApplication app{argc, argv};
     Object object;
-    QLabel widget("Close me :)");
-    QLabel message("Last window was closed");
+    QLabel widget{"Close me :)"};
+    QLabel message{"Last window was closed"};
     QLabel count;
 
     QStateMachine machine;
-    QState * sWindows = new QState(&machine);
-    QState * sSetup = new QState(&machine);
-    QState * sMessage = new QState(&machine);
-    sWindows->assignProperty(qApp, "quitOnLastWindowClosed", false);
-    sWindows->addTransition(qApp, "lastWindowClosed()", sSetup);
-    object.connect(sSetup, SIGNAL(entered()), SLOT(notify()));
+    QState sWindows{&machine};
+    QState sSetup{&machine};
+    QState sMessage{&machine};
+    sWindows.assignProperty(qApp, "quitOnLastWindowClosed", false);
+    sWindows.addTransition(qApp, "lastWindowClosed()", &sSetup);
+    object.connect(&sSetup, SIGNAL(entered()), SLOT(notify()));
     count.connect(&object, SIGNAL(countedTo(int)), SLOT(setNum(int)));
-    sSetup->addTransition(sMessage);
-    sMessage->assignProperty(&message, "visible", true);
-    sMessage->assignProperty(&count, "visible", true);
-    sMessage->assignProperty(qApp, "quitOnLastWindowClosed", true);
-    machine.setInitialState(sWindows);
+    sSetup.addTransition(&sMessage);
+    sMessage.assignProperty(&message, "visible", true);
+    sMessage.assignProperty(&count, "visible", true);
+    sMessage.assignProperty(qApp, "quitOnLastWindowClosed", true);
+    machine.setInitialState(&sWindows);
     machine.start();
     widget.show();
     return app.exec();

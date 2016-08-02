50df5439b5a73302ba8112a40ab5cc5326c67cac
Kuba Ober <kuba@mareimbrium.org>
2016-08-02 10:14:39 -0400

    C++-11-ize, add link, compile on Qt 4 and Qt 5.

--- questions/ncurses-20606318/main.cpp
+++ questions/ncurses-20606318/main.cpp
@@ -1,46 +1,47 @@
-#include <QCoreApplication>
-#include <QSocketNotifier>
-#include <QBasicTimer>
+// https://github.com/KubaO/stackoverflown/tree/master/questions/ncurses-20606318
+#include <QtCore>
 #include <ncurses.h>
 
 class Worker : public QObject
 {
-    Q_OBJECT
-    QBasicTimer m_timer;
-    Q_SLOT void readyRead() {
-        // It's OK to call this with no data available to be read.
-        int c;
-        while ((c = getch()) != ERR) {
-            printw("%c", (char)(c <= 255 ? c : '?'));
-        }
-    }
-    void timerEvent(QTimerEvent * ev) {
-        if (ev->timerId() != m_timer.timerId()) return;
-        printw("*");
-        refresh();
-    }
+   Q_OBJECT
+   QSocketNotifier m_notifier{0, QSocketNotifier::Read, this};
+   QBasicTimer m_timer;
+   Q_SLOT void readyRead() {
+      // It's OK to call this with no data available to be read.
+      int c;
+      while ((c = getch()) != ERR) {
+         printw("%c", (char)(c <= 255 ? c : '?'));
+         if (c == 'q' || c == 'Q') qApp->quit();
+      }
+   }
+   void timerEvent(QTimerEvent * ev) {
+      if (ev->timerId() != m_timer.timerId()) return;
+      printw("*");
+      refresh();
+   }
 public:
-    Worker(QObject * parent = 0) : QObject(parent) {
-        connect(new QSocketNotifier(0, QSocketNotifier::Read, this),
-                SIGNAL(activated(int)), SLOT(readyRead()));
-        readyRead(); // data might be already available without notification
-        m_timer.start(1000, this);
-    }
+   Worker(QObject * parent = 0) : QObject(parent) {
+      connect(&m_notifier, SIGNAL(activated(int)), SLOT(readyRead()));
+      readyRead(); // data might be already available without notification
+      m_timer.start(1000, this);
+   }
 };
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
-    Worker w;
-    WINDOW * win = initscr();
-    clear();
-    cbreak(); // all input is available immediately
-    noecho(); // no echo
-    keypad(win, true); // special keys are interpreted and returned as single int from getch()
-    nodelay(win, true); // getch() is a non-blocking call
-    int rc = a.exec();
-    endwin();
-    return rc;
+   QCoreApplication a{argc, argv};
+   Worker w;
+   auto win = initscr();
+   clear();
+   cbreak(); // all input is available immediately
+   noecho(); // no echo
+   printw("Press <q> to quit\n");
+   keypad(win, true); // special keys are interpreted and returned as single int from getch()
+   nodelay(win, true); // getch() is a non-blocking call
+   auto rc = a.exec();
+   endwin();
+   return rc;
 }
 
 #include "main.moc"
--- questions/ncurses-20606318/ncurses-20606318.pro
+++ questions/ncurses-20606318/ncurses-20606318.pro
@@ -1,19 +1,18 @@
-#-------------------------------------------------
-#
-# Project created by QtCreator 2013-12-17T12:51:57
-#
-#-------------------------------------------------
-
-QT       += core
-
-QT       -= gui
-
+QT = core
+CONFIG += console
+greaterThan(QT_MAJOR_VERSION, 4) {
+    QT += widgets
+    CONFIG += c++11
+} else {
+    unix:QMAKE_CXXFLAGS += -std=c++11
+    macx {
+        QMAKE_CXXFLAGS += -stdlib=libc++
+        QMAKE_MACOSX_DEPLOYMENT_TARGET = 10.7
+    }
+}
 LIBS += -lncurses
 TARGET = ncurses-20606318
 CONFIG   += console
 CONFIG   -= app_bundle
-
 TEMPLATE = app
-
-
 SOURCES += main.cpp

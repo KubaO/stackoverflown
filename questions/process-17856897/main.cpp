39dc58008a7604609ece275a01b6e509b238c4a4
Kuba Ober <kuba@mareimbrium.org>
2016-10-31 09:19:02 -0400

    Modernize.

--- questions/process-17856897/main.cpp
+++ questions/process-17856897/main.cpp
@@ -1,32 +1,29 @@
-#include <QCoreApplication>
-#include <QStringList>
-#include <QBasicTimer>
-#include <QProcess>
-#include <QTextStream>
+// https://github.com/KubaO/stackoverflown/tree/master/questions/process-17856897
+#include <QtCore>
 
-QTextStream out(stdout);
+QTextStream out{stdout};
 
 class Slave : public QObject {
     QBasicTimer m_timer;
-    int m_i;
-    void timerEvent(QTimerEvent * ev) {
+    int m_iter = 0;
+    void timerEvent(QTimerEvent * ev) override {
         if (ev->timerId() == m_timer.timerId()) {
-            out << "iteration " << m_i++ << endl;
-            if (m_i > 35) qApp->quit();
+            out << "iteration " << m_iter++ << endl;
+            if (m_iter > 35) qApp->quit();
         }
     }
 public:
-    Slave(QObject *parent = 0) : QObject(parent), m_i(0) {
+    Slave(QObject *parent = nullptr) : QObject(parent) {
         m_timer.start(100, this);
     }
 };
 
 class Master : public QObject {
     Q_OBJECT
-    QProcess * m_proc;
+    QProcess m_proc{this};
     Q_SLOT void read() {
-        while (m_proc->canReadLine()) {
-            out << "read: " << m_proc->readLine();
+        while (m_proc.canReadLine()) {
+            out << "read: " << m_proc.readLine();
             out.flush(); // endl implicitly flushes, so we must do the same
         }
     }
@@ -38,23 +35,22 @@ class Master : public QObject {
         qApp->quit();
     }
 public:
-    Master(QObject *parent = 0) : QObject(parent), m_proc(new QProcess(this)) {
-        connect(m_proc, SIGNAL(readyRead()), SLOT(read()));
-        connect(m_proc, SIGNAL(started()), SLOT(started()));
-        connect(m_proc, SIGNAL(finished(int)), SLOT(finished()));
-        m_proc->start(qApp->applicationFilePath(), QStringList("dummy"));
+    Master(QObject *parent = nullptr) : QObject(parent) {
+        connect(&m_proc, SIGNAL(readyRead()), SLOT(read()));
+        connect(&m_proc, SIGNAL(started()), SLOT(started()));
+        connect(&m_proc, SIGNAL(finished(int)), SLOT(finished()));
+        m_proc.start(qApp->applicationFilePath(), {"dummy"});
     }
 };
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
-    if (a.arguments().length() > 1) {
-        new Slave(&a); // called with an argument, this is the slave process
-    } else {
-        new Master(&a); // no arguments, this is the master
-    }
-    return a.exec();
+    QCoreApplication app{argc, argv};
+    if (app.arguments().length() > 1)
+        new Slave{&app}; // called with an argument, this is the slave process
+    else
+        new Master{&app}; // no arguments, this is the master
+    return app.exec();
 }
 
 #include "main.moc"
---
+++ questions/process-17856897/process-17856897.pro
@@ -0,0 +1,16 @@
+greaterThan(QT_MAJOR_VERSION, 4) {
+    CONFIG += c++11
+} else {
+    unix:QMAKE_CXXFLAGS += -std=c++11
+    macx {
+        QMAKE_CXXFLAGS += -stdlib=libc++
+        QMAKE_MACOSX_DEPLOYMENT_TARGET = 10.7
+    }
+}
+QT = core
+TARGET = process-17856897
+CONFIG   += console
+CONFIG   -= app_bundle
+TEMPLATE = app
+SOURCES += main.cpp
+

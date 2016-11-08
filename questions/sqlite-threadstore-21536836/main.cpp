1e27df4a91e822c954aa5b93f9289741c2c1a820
Kuba Ober <kuba@mareimbrium.org>
2016-11-08 11:05:52 -0500

    Modernize, make the example actually do something.

--- questions/sqlite-threadstore-21536836/main.cpp
+++ questions/sqlite-threadstore-21536836/main.cpp
@@ -1,59 +1,68 @@
-#include <QCoreApplication>
+// https://github.com/KubaO/stackoverflown/tree/master/questions/sqlite-threadstore-21536836
+#include <QtCore>
 #include <sqlite3.h>
-#include <QThreadStorage>
-#include <QMap>
 
 class SQLiteStmt {
-    sqlite3_stmt* m_stmt;
-    int m_rc;
-    friend class SQLiteDB;
+   Q_DISABLE_COPY(SQLiteStmt)
+   sqlite3_stmt* m_stmt;
+   int m_rc;
+   friend class SQLiteDB;
 private:
-    explicit SQLiteStmt(sqlite3_stmt* stmt, int rc) : m_stmt(stmt), m_rc(rc) {}
+   explicit SQLiteStmt(sqlite3_stmt* stmt, int rc) : m_stmt(stmt), m_rc(rc) {}
 public:
-    ~SQLiteStmt() { sqlite3_finalize(m_stmt); m_stmt = 0; }
-    int status() const { return m_rc; }
-    operator sqlite3_stmt*() const { return m_stmt; }
+   SQLiteStmt(SQLiteStmt && o) : m_stmt(o.m_stmt), m_rc(o.m_rc) {
+      o.m_stmt = nullptr;
+   }
+   ~SQLiteStmt() { sqlite3_finalize(m_stmt); m_stmt = nullptr; }
+   int status() const { return m_rc; }
+   operator sqlite3_stmt*() const { return m_stmt; }
 };
 
 class SQLiteDB {
-  sqlite3* m_db;
-  Q_DISABLE_COPY(SQLiteDB)
+   Q_DISABLE_COPY(SQLiteDB)
+   sqlite3* m_db = nullptr;
 public:
-  SQLiteDB() : m_db(0) {}
-  int open(const QString & filename) {
-    close();
-    return sqlite3_open16(filename.utf16(), &m_db);
-  }
-  int close() {
-    sqlite3* const db = m_db;
-    m_db = 0;
-    if (db) return sqlite3_close(db);
-    return SQLITE_OK;
-  }
-  SQLiteStmt prepare(const QString & sql) {
+   SQLiteDB() = default;
+   int open(const QString & filename) {
+      close();
+      return sqlite3_open16(filename.utf16(), &m_db);
+   }
+   int close() {
+      sqlite3* const db = m_db;
+      m_db = nullptr;
+      if (db) return sqlite3_close(db);
+      return SQLITE_OK;
+   }
+   int exec(const QString & sql) {
+      auto stmt = prepare(sql);
+      int rc = stmt.status();
+      if (rc == SQLITE_OK) do {
+         rc = sqlite3_step(stmt);
+      } while (rc == SQLITE_ROW);
+      return rc;
+   }
+   SQLiteStmt prepare(const QString & sql) {
       sqlite3_stmt* stmt;
-      int rc = sqlite3_prepare16_v2(m_db, sql.utf16(), -1, &stmt, 0);
-      return SQLiteStmt(stmt, rc);
-  }
-  ~SQLiteDB() { close(); }
-  operator sqlite3*() const { return m_db; }
+      auto rc = sqlite3_prepare16_v2(m_db, sql.utf16(), -1, &stmt, nullptr);
+      return SQLiteStmt{stmt, rc};
+   }
+   ~SQLiteDB() { close(); }
+   operator sqlite3*() const { return m_db; }
 };
 
-void run()
-{
-    QThreadStorage<SQLiteDB> dbData;
-    SQLiteDB & db(dbData.localData());
-    db.open(":memory:");
-    int rc = sqlite3_busy_timeout(db, 100);
-    SQLiteStmt stmt = db.prepare("SELECT * FROM myTable");
-    if (stmt.status() == SQLITE_OK) {
-        int rc = sqlite3_step(stmt);
-        // ...
-    }
-}
-
-
-int main(int, char **)
-{
-    return 0;
+int main() {
+   QThreadStorage<SQLiteDB> dbData;
+   SQLiteDB & db(dbData.localData());
+   if (db.open(":memory:") != SQLITE_OK)
+      qDebug() << "open failed";
+   if (db.exec("CREATE TABLE myTable(value NUMERIC)") != SQLITE_DONE)
+      qDebug() << "create failed";
+   if (db.exec("INSERT INTO myTable VALUES (10), (20), (30)") != SQLITE_DONE)
+      qDebug() << "insert failed";
+   auto stmt = db.prepare("SELECT * FROM myTable");
+   if (stmt.status() == SQLITE_OK) while (true) {
+      int rc = sqlite3_step(stmt);
+      if (rc != SQLITE_ROW) break;
+      qDebug() << sqlite3_column_int(stmt, 0);
+   }
 }
--- questions/sqlite-threadstore-21536836/sqlite-threadstore-21536836.pro
+++ questions/sqlite-threadstore-21536836/sqlite-threadstore-21536836.pro
@@ -1,18 +1,7 @@
-#-------------------------------------------------
-#
-# Project created by QtCreator 2014-02-03T17:57:17
-#
-#-------------------------------------------------
-
-QT       += core
-
-QT       -= gui
-
+QT = core
 TARGET = sqlite-threadstore-21536836
 CONFIG   += console
 CONFIG   -= app_bundle
-
 TEMPLATE = app
-LIBS += -L/opt/local/lib -lsqlite3
-
 SOURCES += main.cpp
+macx:LIBS += -L/opt/local/lib -lsqlite3

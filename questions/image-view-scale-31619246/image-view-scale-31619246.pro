3b479e1c2cb90eea9154c165d8ecb0f60c67d7a9
Kuba Ober <kuba@mareimbrium.org>
2017-03-27 10:58:09 -0400

    Modernize.

--- questions/image-view-scale-31619246/image-view-scale-31619246.pro
+++ questions/image-view-scale-31619246/image-view-scale-31619246.pro
@@ -1,4 +1,4 @@
-QT       += widgets network
+QT = widgets network
 CONFIG += c++11
 TARGET = image-view-scale-31619246
 TEMPLATE = app
--- questions/image-view-scale-31619246/main.cpp
+++ questions/image-view-scale-31619246/main.cpp
@@ -1,21 +1,20 @@
+// https://github.com/KubaO/stackoverflown/tree/master/questions/image-view-scale-31619246
 #include <QtWidgets>
 #include <QtNetwork>
 
 class FragTreeViewer : public QWidget {
-   QGridLayout m_layout;
+   QGridLayout m_layout{this};
    QScrollArea m_area;
    QLabel m_imageLabel, m_scaleLabel;
-   QPushButton m_zoomOut, m_zoomIn;
-   double m_scaleFactor;
+   QPushButton m_zoomOut{"Zoom Out"}, m_zoomIn{"Zoom In"};
+   double m_scaleFactor = 1.0;
 public:
    void setImage(const QImage & img) {
       m_scaleFactor = 1.0;
       m_imageLabel.setPixmap(QPixmap::fromImage(img));
       scaleImage(1.0);
    }
-   FragTreeViewer() :
-      m_layout(this), m_zoomOut("Zoom Out"), m_zoomIn("Zoom In"),
-      m_scaleFactor(1.0) {
+   FragTreeViewer() {
       m_layout.addWidget(&m_area, 0, 0, 1, 3);
       m_layout.addWidget(&m_zoomOut, 1, 0);
       m_layout.addWidget(&m_scaleLabel, 1, 1);
@@ -39,7 +38,7 @@ int main(int argc, char *argv[])
    FragTreeViewer viewer;
    QNetworkAccessManager mgr;
    QScopedPointer<QNetworkReply> rsp(
-            mgr.get(QNetworkRequest(QUrl("http://i.imgur.com/ikwUmUV.jpg"))));
+            mgr.get(QNetworkRequest({"http://i.imgur.com/ikwUmUV.jpg"})));
    QObject::connect(rsp.data(), &QNetworkReply::finished, [&]{
       if (rsp->error() == QNetworkReply::NoError)
          viewer.setImage(QImage::fromData(rsp->readAll()));

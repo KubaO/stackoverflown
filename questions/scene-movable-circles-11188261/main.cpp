3c1a13c13f23606a2afba43aca60e3e12bac297d
Kuba Ober <kuba@mareimbrium.org>
2017-03-28 11:46:14 -0400

    Modernize.

--- questions/scene-movable-circles-11188261/main.cpp
+++ questions/scene-movable-circles-11188261/main.cpp
@@ -1,80 +1,68 @@
 // https://github.com/KubaO/stackoverflown/tree/master/questions/scene-movable-circles-11188261
-//main.cpp
+// main.cpp
+#include <QtGui>
+#if QT_VERSION >= QT_VERSION_CHECK(5,0,0)
+#include <QtWidgets>
+#endif
 #include <cmath>
-#include <QGraphicsView>
-#include <QPushButton>
-#include <QGridLayout>
-#include <QGraphicsItem>
-#include <QMouseEvent>
-#include <QApplication>
 
-class Circle : public QGraphicsEllipseItem
-{
-    QBrush brush;
+class Circle : public QGraphicsEllipseItem {
+   QBrush m_inBrush{Qt::red}, m_outBrush{Qt::lightGray};
 public:
-    Circle(const QPointF & c) : brush("lightgray") {
-        const qreal r = 10.0 + (50.0*qrand())/RAND_MAX;
-        setRect(QRectF(c.x()-r, c.y()-r, 2.0*r, 2.0*r));
-        setFlag(QGraphicsItem::ItemIsMovable);
-        setFlag(QGraphicsItem::ItemIsFocusable);
-        setPen(QPen("red"));
-        setBrush(brush);
-    }
-    void focusInEvent(QFocusEvent *) { setBrush(QBrush("red")); }
-    void focusOutEvent(QFocusEvent *) { setBrush(brush); }
+   Circle(const QPointF & c) {
+      const qreal r = 10.0 + (50.0*qrand())/RAND_MAX;
+      setRect({c.x()-r, c.y()-r, 2.0*r, 2.0*r});
+      setFlag(QGraphicsItem::ItemIsMovable);
+      setFlag(QGraphicsItem::ItemIsFocusable);
+      setPen({Qt::red});
+      setBrush(m_outBrush);
+   }
+   void focusInEvent(QFocusEvent *) override { setBrush(m_inBrush); }
+   void focusOutEvent(QFocusEvent *) override { setBrush(m_outBrush); }
 };
 
-class Painter : public QWidget
-{
-    Q_OBJECT
-    QGraphicsView * view;
-    QGraphicsScene * scene;
+class Painter : public QWidget {
+   Q_OBJECT
+   QGridLayout m_layout{this};
+   QGraphicsView m_view;
+   QPushButton m_clear{"Clear"};
+   QPushButton m_remove{"Remove"};
+   QGraphicsScene m_scene;
 public:
-    explicit Painter(QWidget *parent = 0);
-protected slots:
-    void on_remove_clicked();
+   explicit Painter(QWidget *parent = nullptr);
 protected:
-   void mousePressEvent(QMouseEvent *event);
+   Q_SLOT void on_remove_clicked();
+   void mousePressEvent(QMouseEvent *event) override;
 };
 
-Painter::Painter(QWidget *parent) :
-    QWidget(parent),
-    view(new QGraphicsView(this)),
-    scene(new QGraphicsScene(this))
-{
-    QGridLayout * layout = new QGridLayout();
-    layout->addWidget(view, 0, 0, 1, 2);
-    QPushButton * button = new QPushButton("Clear", this);
-    connect(button, SIGNAL(clicked()), scene, SLOT(clear()));
-    layout->addWidget(button, 1, 0);
-    button = new QPushButton("Remove", this);
-    button->setObjectName("remove");
-    layout->addWidget(button, 1, 1);
-    setLayout(layout);
-    QMetaObject::connectSlotsByName(this);
+Painter::Painter(QWidget *parent) : QWidget(parent) {
+   m_layout.addWidget(&m_view, 0, 0, 1, 2);
+   m_layout.addWidget(&m_clear, 1, 0);
+   m_layout.addWidget(&m_remove, 1, 1);
+   m_remove.setObjectName("remove");
+   QMetaObject::connectSlotsByName(this);
+   connect(&m_clear, SIGNAL(clicked()), &m_scene, SLOT(clear()));
 
-    view->setRenderHint(QPainter::Antialiasing);
-    view->setScene(scene);
-    view->setSceneRect(0,0,500,500);
+   m_view.setRenderHint(QPainter::Antialiasing);
+   m_view.setScene(&m_scene);
+   m_view.setSceneRect(0,0,500,500);
 }
 
-void Painter::mousePressEvent(QMouseEvent *event)
-{
-    const QPointF center = view->mapToScene(view->mapFromParent(event->pos()));
-    scene->addItem(new Circle(center));
+void Painter::mousePressEvent(QMouseEvent *event) {
+   auto center = m_view.mapToScene(m_view.mapFromParent(event->pos()));
+   m_scene.addItem(new Circle(center));
 }
 
-void Painter::on_remove_clicked()
-{
-    delete scene->focusItem();
+void Painter::on_remove_clicked() {
+   delete m_scene.focusItem();
 }
 
-int main(int argc, char *argv[])
+int main(int argc, char ** argv)
 {
-    QApplication a(argc, argv);
-    Painter p;
-    p.show();
-    return a.exec();
+   QApplication app{argc, argv};
+   Painter p;
+   p.show();
+   return app.exec();
 }
 
 #include "main.moc"
--- questions/scene-movable-circles-11188261/scene-movable-circles-11188261.pro
+++ questions/scene-movable-circles-11188261/scene-movable-circles-11188261.pro
@@ -8,8 +8,10 @@ greaterThan(QT_MAJOR_VERSION, 4) {
     macx {
         QMAKE_CXXFLAGS += -stdlib=libc++
         QMAKE_MACOSX_DEPLOYMENT_TARGET = 10.7
+        QMAKE_CXXFLAGS_WARN_ON += -Wno-inconsistent-missing-override
     }
 }
 TARGET = scene-movable-circles-11188261
 TEMPLATE = app
 SOURCES += main.cpp
+DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x050800

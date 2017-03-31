a319bd48f60f1b2e7c995ebf11fb07daeab41ce5
Kuba Ober <kuba@mareimbrium.org>
2017-03-31 08:57:26 -0400

    Modernize.

--- questions/filter-18964377/filter-18964377.pro
+++ questions/filter-18964377/filter-18964377.pro
@@ -8,8 +8,10 @@ greaterThan(QT_MAJOR_VERSION, 4) {
     macx {
         QMAKE_CXXFLAGS += -stdlib=libc++
         QMAKE_MACOSX_DEPLOYMENT_TARGET = 10.7
+        QMAKE_CXXFLAGS_WARN_ON += -Wno-inconsistent-missing-override
     }
 }
+DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x050800
 TARGET = filter-18964377
 TEMPLATE = app
 SOURCES += main.cpp
--- questions/filter-18964377/main.cpp
+++ questions/filter-18964377/main.cpp
@@ -1,3 +1,4 @@
+// https://github.com/KubaO/stackoverflown/tree/master/questions/filter-18964377
 #include <QtGui>
 #if QT_VERSION_MAJOR > 4
 #include <QtWidgets>
@@ -17,31 +18,30 @@ public:
 class VehicleModel : public QAbstractTableModel {
    QList<Vehicle> m_data;
 public:
-   VehicleModel(QObject * parent = nullptr) : QAbstractTableModel{parent} {}
+   VehicleModel(QObject * parent = {}) : QAbstractTableModel{parent} {}
    int rowCount(const QModelIndex &) const override { return m_data.count(); }
    int columnCount(const QModelIndex &) const override { return 3; }
    QVariant data(const QModelIndex &index, int role) const override {
-      if (role != Qt::DisplayRole && role != Qt::EditRole) return QVariant{};
-      const Vehicle & vehicle = m_data[index.row()];
+      if (role != Qt::DisplayRole && role != Qt::EditRole) return {};
+      const auto & vehicle = m_data[index.row()];
       switch (index.column()) {
       case 0: return vehicle.make();
       case 1: return vehicle.model();
       case 2: return vehicle.registrationNumber();
-      default: return QVariant{};
+      default: return {};
       };
    }
    QVariant headerData(int section, Qt::Orientation orientation, int role) const override {
-      if (orientation != Qt::Horizontal) return QVariant{};
-      if (role != Qt::DisplayRole) return QVariant{};
+      if (orientation != Qt::Horizontal || role != Qt::DisplayRole) return {};
       switch (section) {
       case 0: return "Make";
       case 1: return "Model";
       case 2: return "Reg.#";
-      default: return QVariant{};
+      default: return {};
       }
    }
    void append(const Vehicle & vehicle) {
-      beginInsertRows(QModelIndex{}, m_data.count(), m_data.count());
+      beginInsertRows({}, m_data.count(), m_data.count());
       m_data.append(vehicle);
       endInsertRows();
    }
@@ -59,10 +59,10 @@ public:
       m_layout.addWidget(&m_view, 0, 0, 1, 1);
       m_layout.addWidget(&m_button, 1, 0, 1, 1);
       connect(&m_button, SIGNAL(clicked()), &m_dialog, SLOT(open()));
-      m_model.append(Vehicle{"Volvo", "240", "SQL8941"});
-      m_model.append(Vehicle{"Volvo", "850", QString{}});
-      m_model.append(Vehicle{"Volvo", "940", "QRZ1321"});
-      m_model.append(Vehicle{"Volvo", "960", "QRZ1628"});
+      m_model.append({"Volvo", "240", "SQL8941"});
+      m_model.append({"Volvo", "850", {}});
+      m_model.append({"Volvo", "940", "QRZ1321"});
+      m_model.append({"Volvo", "960", "QRZ1628"});
       m_proxy.setSourceModel(&m_model);
       m_proxy.setFilterKeyColumn(2);
       m_view.setModel(&m_proxy);

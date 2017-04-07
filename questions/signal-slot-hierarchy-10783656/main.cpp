bd092bbe3555b7e90f60f04f1846d453e4c0a7a3
Kuba Ober <kuba@mareimbrium.org>
2017-04-07 13:03:54 -0400

    Modernize.

--- questions/signal-slot-hierarchy-10783656/main.cpp
+++ questions/signal-slot-hierarchy-10783656/main.cpp
@@ -6,49 +6,40 @@
 
 class Window : public QWidget
 {
+   QSignalMapper m_mapper;
+   QStackedLayout m_stack{this};
+   QWidget      m_page1,                  m_page2;
+   QHBoxLayout  m_layout1{&m_page1},      m_layout2{&m_page2};
+   QLabel       m_label1{"Page 1"},       m_label2{"Page 2"};
+   QPushButton  m_button1{"Show Page 2"}, m_button2{"Show Page 1"};
 public:
-    Window() : QWidget() {
-        QSignalMapper * mapper = new QSignalMapper(this);
-        QWidget * page, * button;
-        QLayout * layout;
+   Window(QWidget * parent = {}) : QWidget(parent) {
+      // the mapper tells the stack which page to switch to
+      connect(&m_mapper, SIGNAL(mapped(int)), &m_stack, SLOT(setCurrentIndex(int)));
 
-        QStackedLayout * stack = new QStackedLayout;
-        // the mapper tells the stack which page to switch to
-        connect(mapper, SIGNAL(mapped(int)), stack, SLOT(setCurrentIndex(int)));
+      // Page 1
+      m_layout1.addWidget(&m_label1);
+      m_layout1.addWidget(&m_button1);
+      // tell the mapper to map signals coming from this button to integer 1 (index of page 2)
+      m_mapper.setMapping(&m_button1, 1);
+      // when the button is clicked, the mapper will do its mapping and emit the mapped() signal
+      connect(&m_button1, SIGNAL(clicked()), &m_mapper, SLOT(map()));
+      m_stack.addWidget(&m_page1);
 
-        // Page 1
-        page = new QWidget(this);
-        layout = new QHBoxLayout;
-        layout->addWidget(new QLabel("Page 1", page));
-        button = new QPushButton("Show Page 2", page);
-        // tell the mapper to map signals coming from this button to integer 1 (index of page 2)
-        mapper->setMapping(button, 1);
-        // when the button is clicked, the mapper will do its mapping and emit the mapped() signal
-        connect(button, SIGNAL(clicked()), mapper, SLOT(map()));
-        layout->addWidget(button);
-        page->setLayout(layout);
-        stack->addWidget(page);
-
-        // Page 2
-        page = new QWidget(this);
-        layout = new QHBoxLayout;
-        layout->addWidget(new QLabel("Page 2", page));
-        button = new QPushButton("Show Page 1", page);
-        // tell the mapper to map signals coming from this button to integer 0 (index of page 1)
-        mapper->setMapping(button, 0);
-        connect(button, SIGNAL(clicked()), mapper, SLOT(map()));
-        layout->addWidget(button);
-        page->setLayout(layout);
-        stack->addWidget(page);
-
-        setLayout(stack);
-    }
+      // Page 2
+      m_layout2.addWidget(&m_label2);
+      m_layout2.addWidget(&m_button2);
+      // tell the mapper to map signals coming from this button to integer 0 (index of page 1)
+      m_mapper.setMapping(&m_button2, 0);
+      connect(&m_button2, SIGNAL(clicked()), &m_mapper, SLOT(map()));
+      m_stack.addWidget(&m_page2);
+   }
 };
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    Window w;
-    w.show();
-    return a.exec();
+   QApplication a(argc, argv);
+   Window w;
+   w.show();
+   return a.exec();
 }

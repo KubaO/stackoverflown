d3802783d524f24fef61c4fd11c67eff3f11e080
Kuba Ober <kuba@mareimbrium.org>
2017-04-19 10:56:10 -0400

    Modernize.

--- questions/scenemod-11232425/main.cpp
+++ questions/scenemod-11232425/main.cpp
@@ -1,21 +1,16 @@
-//main.cpp
-#include <QApplication>
-#include <QGraphicsScene>
-#include <QGraphicsView>
-#include <QGraphicsEllipseItem>
-#include <QGraphicsRectItem>
-#include <QMouseEvent>
-#include <QPointer>
+// https://github.com/KubaO/stackoverflown/tree/master/questions/scenemod-11232425
+#include <QtWidgets>
+
+const char kNotifier[] = "notifier";
 
 class Notifier : public QObject
 {
    Q_OBJECT
-   int m_count;
+   int m_count = {};
 public:
-   Notifier() : m_count(0) {}
    int count() const { return m_count; }
    void inc() { m_count ++; }
-   void notify() { m_count = 0; emit notification(); }
+   void notify() { m_count = {}; emit notification(); }
    Q_SIGNAL void notification();
 };
 
@@ -25,13 +20,13 @@ Q_DECLARE_METATYPE(NotifierPointer)
 template <typename T> class NotifyingItem : public T
 {
 protected:
-   QVariant itemChange(QGraphicsItem::GraphicsItemChange change, const QVariant &value) {
+   QVariant itemChange(QGraphicsItem::GraphicsItemChange change, const QVariant &value) override {
       QVariant v;
       if (change == T::ItemPositionHasChanged &&
           this->scene() &&
-          (v=this->scene()->property("notifier")).isValid())
+          (v=this->scene()->property(kNotifier)).isValid())
       {
-         NotifierPointer notifier = v.value<NotifierPointer>();
+         auto notifier = v.value<NotifierPointer>();
          notifier->inc();
          if (notifier->count() >= this->scene()->selectedItems().count()) {
             notifier->notify();
@@ -41,104 +36,92 @@ protected:
    }
 };
 
-//
 // Note that all you need to make Circle a notifying item is to derive from
 // NotifyingItem<basetype>.
 
 class Circle : public NotifyingItem<QGraphicsEllipseItem>
 {
-   QBrush brush;
+   QBrush m_brush;
 public:
-   Circle(const QPointF & c) : brush(Qt::lightGray) {
+   Circle(const QPointF & c) : m_brush(Qt::lightGray) {
       const qreal r = 10.0 + (50.0*qrand())/RAND_MAX;
-      setRect(QRectF(-r, -r, 2.0*r, 2.0*r));
+      setRect({-r, -r, 2.0*r, 2.0*r});
       setPos(c);
       setFlags(QGraphicsItem::ItemIsMovable | QGraphicsItem::ItemIsSelectable |
                QGraphicsItem::ItemSendsGeometryChanges);
-      setPen(QPen(Qt::red));
-      setBrush(brush);
+      setPen({Qt::red});
+      setBrush(m_brush);
    }
-
 };
 
 class View : public QGraphicsView
 {
    Q_OBJECT
    QGraphicsScene scene;
+   QGraphicsSimpleTextItem text;
+   QGraphicsRectItem centroid{-5, -5, 10, 10};
    Notifier notifier;
-   QGraphicsSimpleTextItem * text;
-   QGraphicsRectItem * centroid;
-   int deltaCounter;
+   int deltaCounter = {};
 public:
-   explicit View(QWidget *parent = 0);
-   ~View() {
-      scene.disconnect(this);
-      // Otherwise gotUpdates will try to access the destroyed centroid while the scene's
-      // destructor is running and the children, inluding the centroid, are already dead.
-   }
+   explicit View(QWidget *parent = {});
 protected:
    Q_SLOT void gotUpdates();
-   void mousePressEvent(QMouseEvent *event);
+   void mousePressEvent(QMouseEvent *event) override;
 };
 
-View::View(QWidget *parent) :
-   QGraphicsView(parent),
-   text(new QGraphicsSimpleTextItem()),
-   centroid(new QGraphicsRectItem(-5, -5, 10, 10)),
-   deltaCounter(0)
+View::View(QWidget *parent) : QGraphicsView(parent)
 {
-   centroid->hide();
-   centroid->setRotation(45.0);
-   centroid->setPen(QPen(Qt::blue));
-   centroid->setZValue(2);
-   scene.addItem(centroid);
-   text->setPos(5, 470);
-   text->setZValue(1);
-   scene.addItem(text);
+   centroid.hide();
+   centroid.setRotation(45.0);
+   centroid.setPen({Qt::blue});
+   centroid.setZValue(2);
+   scene.addItem(&centroid);
+   text.setPos(5, 470);
+   text.setZValue(1);
+   scene.addItem(&text);
    setRenderHint(QPainter::Antialiasing);
    setScene(&scene);
    setSceneRect(0,0,500,500);
-   scene.setProperty("notifier", QVariant::fromValue(NotifierPointer(&notifier)));
-   connect(&notifier, SIGNAL(notification()), SLOT(gotUpdates()));
-   connect(&scene, SIGNAL(selectionChanged()), SLOT(gotUpdates()));
+   scene.setProperty(kNotifier, QVariant::fromValue(NotifierPointer(&notifier)));
+   connect(&notifier, &Notifier::notification, this, &View::gotUpdates);
+   connect(&scene, &QGraphicsScene::selectionChanged, &notifier, &Notifier::notification);
 }
 
 void View::gotUpdates()
 {
    if (scene.selectedItems().isEmpty()) {
-      this->centroid->hide();
+      centroid.hide();
       return;
    }
-   this->centroid->show();
-   QPointF centroid(0.0, 0.0);
-   qreal area = 0.0;
-   foreach (QGraphicsItem * item, scene.selectedItems()) {
+   centroid.show();
+   QPointF centroid;
+   qreal area = {};
+   for (auto item : scene.selectedItems()) {
       const QRectF r = item->boundingRect();
       const qreal a = r.width() * r.height();
       centroid += item->pos() * a;
       area += a;
    }
-   centroid /= area;
-   QString st = QString("delta #%1 with %2 items, centroid at %3, %4")
+   if (area > 0) centroid /= area;
+   auto st = QStringLiteral("delta #%1 with %2 items, centroid at %3, %4")
          .arg(deltaCounter++).arg(scene.selectedItems().count())
          .arg(centroid.x(), 0, 'f', 1).arg(centroid.y(), 0, 'f', 1);
-   this->centroid->setPos(centroid);
-   text->setText(st);
+   this->centroid.setPos(centroid);
+   text.setText(st);
 }
 
 void View::mousePressEvent(QMouseEvent *event)
 {
-   const QPointF center = mapToScene(event->pos());
-   if (! scene.itemAt(center, QTransform())) scene.addItem(new Circle(center));
+   const auto center = mapToScene(event->pos());
+   if (! scene.itemAt(center, {})) scene.addItem(new Circle{center});
    QGraphicsView::mousePressEvent(event);
 }
 
 int main(int argc, char *argv[])
 {
-   QApplication a(argc, argv);
+   QApplication app{argc, argv};
    View v;
    v.show();
-   return a.exec();
+   return app.exec();
 }
-
 #include "main.moc"
--- questions/scenemod-11232425/scenemod.pro
+++ questions/scenemod-11232425/scenemod.pro
@@ -1,4 +1,6 @@
-QT       += widgets
-TARGET = scenemod
+QT = widgets
+CONFIG += c++11
+DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x050800
+TARGET = scenemod-11232425
 TEMPLATE = app
-SOURCES += main.cpp
+SOURCES = main.cpp

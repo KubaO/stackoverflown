c85a5b408fd2484c9f55f24504b40cc05d0a8250
Kuba Ober <kuba@mareimbrium.org>
2017-06-05 16:56:46 -0400

    Modernize.

--- questions/threaded-server-19280903/main.cpp
+++ questions/threaded-server-19280903/main.cpp
@@ -1,84 +1,80 @@
-#include <QCoreApplication>
-#include <QThread>
-#include <QTcpServer>
-#include <QTcpSocket>
-#include <QAbstractEventDispatcher>
-#include <QPointer>
-
-#if QT_VERSION < QT_VERSION_CHECK(5,0,0)
-#define Q_DECL_OVERRIDE override
-#endif
+// https://github.com/KubaO/stackoverflown/tree/master/questions/threaded-server-19280903
+#include <QtNetwork>
+#include <functional>
 
 // A QThread that quits its event loop upon destruction,
 // and waits for the loop to finish.
 class StoppingThread : public QThread {
-    Q_OBJECT
+   Q_OBJECT
 public:
-    StoppingThread(QObject * parent = 0) : QThread(parent) {}
-    ~StoppingThread() { quit(); wait(); qDebug() << this; }
+   using QThread::QThread;
+   ~StoppingThread() { quit(); wait(); qDebug() << this; }
 };
 
 // Deletes an object living in a thread upon thread's termination.
 class ThreadedQObjectDeleter : public QObject {
-    Q_OBJECT
-    QPointer<QObject> m_object;
-    ThreadedQObjectDeleter(QObject * object, QThread * thread) :
-        QObject(thread), m_object(object) {}
-    ~ThreadedQObjectDeleter() {
-        if (m_object && m_object->thread() == 0) {
-            delete m_object;
-        }
-    }
+   Q_OBJECT
+   QPointer<QObject> m_object;
+   ThreadedQObjectDeleter(QObject * object, QThread * thread) :
+      QObject(thread), m_object(object) {}
+   ~ThreadedQObjectDeleter() {
+      if (m_object && m_object->thread() == 0)
+         delete m_object;
+   }
 public:
-    static void addDeleter(QObject * object, QThread * thread) {
-        // The object must not be in the thread yet, otherwise we'd have
-        // a race condition.
-        Q_ASSERT(thread != object->thread());
-        new ThreadedQObjectDeleter(object, thread);
-    }
+   static void addDeleter(QObject * object, QThread * thread) {
+      // The object must not be in the thread yet, otherwise we'd have
+      // a race condition.
+      Q_ASSERT(thread != object->thread());
+      new ThreadedQObjectDeleter(object, thread);
+   }
 };
 
 // Creates servers whenever the listening server gets a new connection
 class ServerFactory : public QObject {
-    Q_OBJECT
-    QMetaObject m_server;
+   Q_OBJECT
+   std::function<QObject*(QTcpSocket*, QObject*)> m_factory;
 public:
-    ServerFactory(const QMetaObject & client, QObject * parent = 0) :
-        QObject(parent), m_server(client) {}
-    Q_SLOT void newConnection() {
-        QTcpServer * listeningServer = qobject_cast<QTcpServer*>(sender());
-        if (!listeningServer) return;
-        QTcpSocket * socket = listeningServer->nextPendingConnection();
-        if (!socket) return;
-        makeServerFor(socket);
-    }
+   template <typename T> struct Constructor {
+      template <typename... Args> T* operator()(Args &&... args) {
+         return new T(std::forward<Args>(args)...);
+      }
+   };
+   template <typename F> ServerFactory(F && factory, QObject * parent = {}) :
+      QObject(parent), m_factory(std::forward<F>(factory)) {}
+   Q_SLOT void newConnection() {
+      auto listeningServer = qobject_cast<QTcpServer*>(sender());
+      if (!listeningServer) return;
+      auto socket = listeningServer->nextPendingConnection();
+      if (!socket) return;
+      makeServerFor(socket);
+   }
 protected:
-    virtual QObject * makeServerFor(QTcpSocket * socket) {
-        QObject * server = m_server.newInstance(Q_ARG(QTcpSocket*, socket), Q_ARG(QObject*, 0));
-        socket->setParent(server);
-        return server;
-    }
+   virtual QObject * makeServerFor(QTcpSocket * socket) {
+      auto server = m_factory(socket, {});
+      socket->setParent(server);
+      return server;
+   }
 };
 
 // A server factory that makes servers in individual threads.
 // The threads automatically delete itselves upon finishing.
 // Destructing the thread also deletes the server.
 class ThreadedServerFactory : public ServerFactory {
-    Q_OBJECT
+   Q_OBJECT
 public:
-    ThreadedServerFactory(const QMetaObject & client, QObject * parent = 0) :
-        ServerFactory(client, parent) {}
+   using ServerFactory::ServerFactory;
 protected:
-    QObject * makeServerFor(QTcpSocket * socket) Q_DECL_OVERRIDE {
-        QObject * server = ServerFactory::makeServerFor(socket);
-        QThread * thread = new StoppingThread(this);
-        connect(thread, SIGNAL(finished()), thread, SLOT(deleteLater()));
-        connect(server, SIGNAL(destroyed()), thread, SLOT(quit()));
-        ThreadedQObjectDeleter::addDeleter(server, thread);
-        server->moveToThread(thread);
-        thread->start();
-        return server;
-    }
+   QObject * makeServerFor(QTcpSocket * socket) override {
+      auto server = ServerFactory::makeServerFor(socket);
+      auto thread = new StoppingThread(this);
+      connect(thread, SIGNAL(finished()), thread, SLOT(deleteLater()));
+      connect(server, SIGNAL(destroyed()), thread, SLOT(quit()));
+      ThreadedQObjectDeleter::addDeleter(server, thread);
+      server->moveToThread(thread);
+      thread->start();
+      return server;
+   }
 };
 
 // A telnet server with following functionality:
@@ -87,43 +83,40 @@ protected:
 // 3. It quits the server upon ^C
 // 4. It disconnects upon receiving 'Q'
 class TelnetServer : public QObject {
-    Q_OBJECT
-    QTcpSocket * m_socket;
-    bool m_firstInput;
-    Q_SLOT void readyRead() {
-        const QByteArray data = m_socket->readAll();
-        if (m_firstInput) {
-            QTextStream out(m_socket);
-            out << "Welcome from thread " << thread() << endl;
-            m_firstInput = false;
-        }
-        for (int i = 0; i < data.length(); ++ i) {
-            char c = data[i];
-            if (c == '\004') /* ^D */ { m_socket->close(); break; }
-            if (c == 'Q') { QCoreApplication::exit(0); break; }
-            m_socket->putChar(c);
-            if (c == '\r') m_socket->write("\r\n:)", 4);
-        }
-        m_socket->flush();
-    }
+   Q_OBJECT
+   QTcpSocket * m_socket;
+   bool m_firstInput = true;
+   Q_SLOT void readyRead() {
+      auto const data = m_socket->readAll();
+      if (m_firstInput) {
+         QTextStream(m_socket) << "Welcome from thread " << thread() << endl;
+         m_firstInput = false;
+      }
+      for (auto c : data) {
+         if (c == '\004') /* ^D */ { m_socket->close(); break; }
+         if (c == 'Q') { QCoreApplication::exit(0); break; }
+         m_socket->putChar(c);
+         if (c == '\r') m_socket->write("\r\n:)", 4);
+      }
+      m_socket->flush();
+   }
 public:
-    Q_INVOKABLE TelnetServer(QTcpSocket * socket, QObject * parent = 0) :
-        QObject(parent), m_socket(socket), m_firstInput(true)
-    {
-        connect(m_socket, SIGNAL(readyRead()), SLOT(readyRead()));
-        connect(m_socket, SIGNAL(disconnected()), SLOT(deleteLater()));
-    }
-    ~TelnetServer() { qDebug() << this; }
+   TelnetServer(QTcpSocket * socket, QObject * parent = {}) :
+      QObject(parent), m_socket(socket)
+   {
+      connect(m_socket, SIGNAL(readyRead()), SLOT(readyRead()));
+      connect(m_socket, SIGNAL(disconnected()), SLOT(deleteLater()));
+   }
+   ~TelnetServer() { qDebug() << this; }
 };
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
-    QTcpServer server;
-    ThreadedServerFactory factory(TelnetServer::staticMetaObject);
-    factory.connect(&server, SIGNAL(newConnection()), SLOT(newConnection()));
-    server.listen(QHostAddress::Any, 8023);
-    return a.exec();
+   QCoreApplication app(argc,  argv);
+   QTcpServer server;
+   ThreadedServerFactory factory((ThreadedServerFactory::Constructor<TelnetServer>()));
+   factory.connect(&server, SIGNAL(newConnection()), SLOT(newConnection()));
+   server.listen(QHostAddress::Any, 8023);
+   return app.exec();
 }
-
 #include "main.moc"
--- questions/threaded-server-19280903/threaded-server-19280903.pro
+++ questions/threaded-server-19280903/threaded-server-19280903.pro
@@ -1,12 +1,15 @@
-QT       += core network
-
-QT       -= gui
-
+greaterThan(QT_MAJOR_VERSION, 4) {
+    CONFIG += c++11
+} else {
+    unix:QMAKE_CXXFLAGS += -std=c++11
+    macx {
+        QMAKE_CXXFLAGS += -stdlib=libc++
+        QMAKE_MACOSX_DEPLOYMENT_TARGET = 10.7
+        QMAKE_CXXFLAGS_WARN_ON += -Wno-inconsistent-missing-override
+    }
+}
+QT = network
+DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x050800
 TARGET = threaded-server-19280903
-CONFIG   += console c++11
-CONFIG   -= app_bundle
-
 TEMPLATE = app
-
-
 SOURCES += main.cpp

05b0aca2768882a59cdcf04b3f777cd21b0319af
Kuba Ober <kuba@mareimbrium.org>
2018-04-10 13:00:31 -0400

    Handle modifiers in the keyname example.

--- questions/keyname-21764138/keyname-21764138.pro
+++ questions/keyname-21764138/keyname-21764138.pro
@@ -1,18 +1,6 @@
-#-------------------------------------------------
-#
-# Project created by QtCreator 2014-02-13T17:21:04
-#
-#-------------------------------------------------
-
-QT       += core
-
-QT       -= gui
-
+QT = core
 TARGET = keyname-21764138
-CONFIG   += console
-CONFIG   -= app_bundle
-
+CONFIG += console
+CONFIG -= app_bundle
 TEMPLATE = app
-
-
 SOURCES += main.cpp
--- questions/keyname-21764138/main.cpp
+++ questions/keyname-21764138/main.cpp
@@ -1,17 +1,39 @@
+// https://github.com/KubaO/stackoverflown/tree/master/questions/keyname-21764138
 #include <QMetaEnum>
 
-class KeyHelper : private QObject {
-public:
-   static QString keyName(int index) {
-      static int keyEnumIndex = staticQtMetaObject.indexOfEnumerator("Key");
-      QString name = staticQtMetaObject.enumerator(keyEnumIndex).valueToKey(index);
-      if (index >= Qt::Key_Left && index <= Qt::Key_Down) name += " Arrow";
-      return name.isEmpty() ? QString() : name.mid(4);
-   }
-};
+namespace SO {
+enum KeyNameOption { KeyNameNone = 0, AppendArrow = 1 };
+Q_DECLARE_FLAGS(KeyNameOptions, KeyNameOption)
+}
+QString keyName(int index, SO::KeyNameOptions opt = {}) {
+   constexpr static auto const getEnum = [](const char *name) {
+      int enumIndex = qt_getQtMetaObject()->indexOfEnumerator(name);
+      return qt_getQtMetaObject()->enumerator(enumIndex);
+   };
+   static const auto keyEnum = getEnum("Key");
+   static const auto modifierEnum = getEnum("KeyboardModifiers");
 
-int main()
-{
-   Q_ASSERT(KeyHelper::keyName(Qt::Key_Tab) == "Tab");
-   Q_ASSERT(KeyHelper::keyName(Qt::Key_Up) == "Up Arrow");
+   auto name = modifierEnum.valueToKeys(index & Qt::KeyboardModifierMask);
+   index &= ~Qt::KeyboardModifierMask;
+
+   if (name == "NoModifier")
+      name.clear();
+   else {
+      name.replace('|', '+');
+      name.replace("Modifier", "");
+      name.append('+');
+   }
+
+   auto keyName = keyEnum.valueToKey(index);
+   if (keyName)
+      name.append(keyName + 4);
+   if ((opt & SO::AppendArrow) && index >= Qt::Key_Left && index <= Qt::Key_Down)
+      name.append(" Arrow");
+   return QLatin1String(name);
+}
+
+int main() {
+   Q_ASSERT(keyName(Qt::Key_Tab) == "Tab");
+   Q_ASSERT(keyName(Qt::ShiftModifier | Qt::Key_Up, SO::AppendArrow) == "Shift+Up Arrow");
+   Q_ASSERT(keyName(Qt::AltModifier | Qt::Key_Down) == "Alt+Down");
 }

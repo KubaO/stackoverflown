c1a1fc8d07aba1b928c40f9b42863c738978f653
Kuba Ober <kuba@mareimbrium.org>
2018-04-12 13:10:43 -0400

    Modernize.

--- questions/pixmap-to-json-32376119/main.cpp
+++ questions/pixmap-to-json-32376119/main.cpp
@@ -1,23 +1,23 @@
-#include <QtWidgets>
+// https://github.com/KubaO/stackoverflown/tree/master/questions/pixmap-to-json-32376119
+#include <QtGui>
 
-QJsonValue jsonValFromPixmap(const QPixmap & p) {
-  QByteArray data;
-  QBuffer buffer { &data };
+QJsonValue jsonValFromPixmap(const QPixmap &p) {
+  QBuffer buffer;
   buffer.open(QIODevice::WriteOnly);
   p.save(&buffer, "PNG");
-  auto encoded = buffer.data().toBase64();
-  return QJsonValue(QString::fromLatin1(encoded));
+  auto const encoded = buffer.data().toBase64();
+  return {QLatin1String(encoded)};
 }
 
-QPixmap pixmapFrom(const QJsonValue & val) {
-  QByteArray encoded = val.toString().toLatin1();
+QPixmap pixmapFrom(const QJsonValue &val) {
+  auto const encoded = val.toString().toLatin1();
   QPixmap p;
   p.loadFromData(QByteArray::fromBase64(encoded), "PNG");
   return p;
 }
 
-int main(int argc, char ** argv) {
-   QApplication app{argc, argv};
+int main(int argc, char **argv) {
+   QGuiApplication app{argc, argv};
    QImage img{32, 32, QImage::Format_RGB32};
    img.fill(Qt::red);
    auto pix = QPixmap::fromImage(img);
@@ -26,4 +26,3 @@ int main(int argc, char ** argv) {
    auto img2 = pix2.toImage();
    Q_ASSERT(img == img2);
 }
-
--- questions/pixmap-to-json-32376119/pixmap-to-json-32376119.pro
+++ questions/pixmap-to-json-32376119/pixmap-to-json-32376119.pro
@@ -1,5 +1,6 @@
-QT = widgets 
+QT = gui
 CONFIG += c++11
 TARGET = pixmap-to-json-32376119
 TEMPLATE = app
 SOURCES += main.cpp
+DEFINES += QT_NO_CAST_FROM_ASCII

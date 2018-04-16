1c87bc5fd575d043df4038d13d335036f3444090
Kuba Ober <kuba@mareimbrium.org>
2018-04-16 11:37:23 -0400

    Add url.

--- questions/appicon-18560355/appicon-18560355.pro
+++ questions/appicon-18560355/appicon-18560355.pro
@@ -1,4 +1,4 @@
-QT       += gui
+QT += gui
 greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
 TARGET = appicon-18560355
 TEMPLATE = app
--- questions/appicon-18560355/main.cpp
+++ questions/appicon-18560355/main.cpp
@@ -1,35 +1,34 @@
-#include <QFontDialog>
-#include <QApplication>
-#include <QIcon>
-#include <QPainter>
+// https://github.com/KubaO/stackoverflown/tree/master/questions/appicon-18560355
+#include <QtGui>
+#if QT_VERSION >= QT_VERSION_CHECK(5,0,0)
+#include <QtWidgets>
+#endif
 
 class Dialog : public QFontDialog {
 public:
-    Dialog(QWidget *parent = 0) : QFontDialog(parent) {}
-    Dialog(const QFont & initial, QWidget *parent = 0) : QFontDialog(initial, parent) {}
+   Dialog(QWidget *parent = 0) : QFontDialog(parent) {}
+   Dialog(const QFont & initial, QWidget *parent = 0) : QFontDialog(initial, parent) {}
 };
 
-QIcon myIcon(const QColor & color)
-{
-    QIcon icon;
-    QPixmap pm(128, 128);
-    pm.fill(Qt::transparent);
-    QPainter p(&pm);
-    p.translate(64, 64);
-    p.scale(50, 50);
-    p.setBrush(color);
-    p.setPen(QPen(Qt::lightGray, 0.1));
-    p.drawEllipse(-1, -1, 2, 2);
-    icon.addPixmap(pm);
-    return icon;
+QIcon myIcon(const QColor & color) {
+   QIcon icon;
+   QPixmap pm(128, 128);
+   pm.fill(Qt::transparent);
+   QPainter p(&pm);
+   p.translate(64, 64);
+   p.scale(50, 50);
+   p.setBrush(color);
+   p.setPen(QPen(Qt::lightGray, 0.1));
+   p.drawEllipse(-1, -1, 2, 2);
+   icon.addPixmap(pm);
+   return icon;
 }
 
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
-    a.setWindowIcon(myIcon(Qt::red));
-    Dialog d;
-    d.setWindowIcon(myIcon(Qt::blue));
-    d.show();
-    return a.exec();
+int main(int argc, char *argv[]) {
+   QApplication a(argc, argv);
+   a.setWindowIcon(myIcon(Qt::red));
+   Dialog d;
+   d.setWindowIcon(myIcon(Qt::blue));
+   d.show();
+   return a.exec();
 }

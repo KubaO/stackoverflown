3892cce0a9ed5f828f40880e844bf1751085934b
Kuba Ober <kuba@bertec.com>
2018-05-03 13:22:37 -0400

    Tweak the implementation, make it work on Windows.

--- questions/appmonitor-37524491/main.cpp
+++ questions/appmonitor-37524491/main.cpp
@@ -1,71 +1,90 @@
 // https://github.com/KubaO/stackoverflown/tree/master/questions/appmonitor-37524491
 #include <QtWidgets>
 #include <cstdlib>
-
-static int businessLogicMain(int &argc, char **argv) {
-  QApplication app{argc, argv};
-  QWidget w;
-  QHBoxLayout layout{&w};
-  QPushButton crash{"Crash"};  // purposefully crash for testing
-  QPushButton quit{"Quit"};    // graceful exit, which doesn't need restart
-  layout.addWidget(&crash);
-  layout.addWidget(&quit);
-  w.show();
-
-  QObject::connect(&crash, &QPushButton::clicked, []{ abort(); });
-  QObject::connect(&quit, &QPushButton::clicked, &app, &QCoreApplication::quit);
-  return app.exec();
-}
-
-static auto const kRunLogic = QStringLiteral("run__business__logic");
-static auto const kRunLogicValue = kRunLogic;
-
 #if defined(Q_OS_WIN32)
 #include <windows.h>
-QString getWindowsCommandLine() {
-  return QString::fromWCharArray(GetCommandLine());
+#else
+static void DebugBreak() { abort(); }
+#endif
+
+static int businessLogicMain(int &argc, char **argv) {
+   QApplication app{argc, argv};
+   qDebug() << __FUNCTION__ << app.arguments();
+   QWidget w;
+   QHBoxLayout layout{&w};
+   QPushButton crash{"Crash"};  // purposefully crash for testing
+   QPushButton quit{"Quit"};    // graceful exit, which doesn't need restart
+   layout.addWidget(&crash);
+   layout.addWidget(&quit);
+   w.show();
+
+   QObject::connect(&crash, &QPushButton::clicked, DebugBreak);
+   QObject::connect(&quit, &QPushButton::clicked, &QCoreApplication::quit);
+   return app.exec();
 }
-int	setenv(const char *name, const char *value, int) {
-  return _putenv_s(name, value);
+
+static char const kRunLogic[] = "run__business__logic";
+static char const kRunLogicValue[] = "run__business__logic";
+
+#if defined(Q_OS_WIN32)
+static QString getWindowsCommandLineArguments() {
+   const wchar_t *args = GetCommandLine();
+   bool oddBackslash = false, quoted = false, whitespace = false;
+   // skip the executable name according to Windows command line parsing rules
+   while (auto c = *args) {
+      if (c == L'\\')
+         oddBackslash ^= 1;
+      else if (c == L'"')
+         quoted ^= !oddBackslash;
+      else if (c == L' ' || c == L'\t')
+         whitespace = !quoted;
+      else if (whitespace)
+         break;
+      else
+         oddBackslash = false;
+      args++;
+   }
+   return QString::fromRawData(reinterpret_cast<const QChar*>(args), lstrlen(args));
 }
 #endif
 
 static int monitorMain(int &argc, char **argv) {
-  QCoreApplication app{argc, argv};
-  QProcess proc;
-  auto onFinished = [&](int retcode, QProcess::ExitStatus status) {
-    if (status == QProcess::CrashExit)
-      proc.start();      // restart the app if the app crashed
-    else
-      app.exit(retcode); // no restart required
-  };
-  QObject::connect(&proc, qOverload<int, QProcess::ExitStatus>(&QProcess::finished), onFinished);
-
-  auto env = QProcessEnvironment::systemEnvironment();
-  env.insert(kRunLogic, kRunLogicValue);
-  proc.setProgram(app.applicationFilePath()); // logic and monitor are the same executable
-#if defined(Q_OS_WIN32)
-  proc.setNativeArguments(getWindowsCommandLine());
-#else
-  proc.setArguments(app.arguments().mid(1));
+#if !defined(Q_OS_WIN32)
+   QStringList args;
+   args.reserve(argc-1);
+   for (int i = 1; i < argc; ++i)
+     args << QString::fromLocal8Bit(argv[i]);
 #endif
-  proc.setProcessEnvironment(env);
-  proc.setProcessChannelMode(QProcess::ForwardedChannels);
-  proc.start();
-  return app.exec();
-}
+   QCoreApplication app{argc, argv};
+   QProcess proc;
+   auto onFinished = [&](int retcode, QProcess::ExitStatus status) {
+      qDebug() << status;
+      if (status == QProcess::CrashExit)
+         proc.start();      // restart the app if the app crashed
+      else
+         app.exit(retcode); // no restart required
+   };
+   QObject::connect(&proc, QOverload<int, QProcess::ExitStatus>::of(&QProcess::finished), onFinished);
 
-static bool checkForRunLogic() {
-  auto hasRunLogic = QProcessEnvironment::systemEnvironment().value(kRunLogic) == kRunLogicValue;
-  if (hasRunLogic)
-    setenv(kRunLogic.toLocal8Bit(), "", true);
-  return hasRunLogic;
+   auto env = QProcessEnvironment::systemEnvironment();
+   env.insert(kRunLogic, kRunLogicValue);
+   proc.setProgram(app.applicationFilePath()); // logic and monitor are the same executable
+#if defined(Q_OS_WIN32)
+   SetErrorMode(SEM_NOGPFAULTERRORBOX);        // disable Windows error reporting
+   proc.setNativeArguments(getWindowsCommandLineArguments()); // pass command line arguments natively
+   env.insert("QT_LOGGING_TO_CONSOLE", "1");   // ensure that the debug output gets passed along
+#else
+   proc.setArguments(args);
+#endif
+   proc.setProcessEnvironment(env);
+   proc.setProcessChannelMode(QProcess::ForwardedChannels);
+   proc.start();
+   return app.exec();
 }
 
 int main(int argc, char **argv) {
-  if (!checkForRunLogic())
-    return monitorMain(argc, argv);
-  else
-    return businessLogicMain(argc, argv);
+   if (qgetenv(kRunLogic) != kRunLogicValue)
+      return monitorMain(argc, argv);
+   else
+      return qunsetenv(kRunLogic), businessLogicMain(argc, argv);
 }
-

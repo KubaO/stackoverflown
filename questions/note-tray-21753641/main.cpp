ac41e6dc91dd0698dce7002104e5c68c698b8cb3
Kuba Ober <kuba@mareimbrium.org>
2018-05-17 15:18:46 -0400

    Tweak

--- questions/note-tray-21753641/main.cpp
+++ questions/note-tray-21753641/main.cpp
@@ -1,10 +1,6 @@
-#include <QApplication>
-#include <QSystemTrayIcon>
-#include <QIcon>
-#include <QPixmap>
-#include <QMenu>
-#include <QList>
-#include <QPointer>
+// https://github.com/KubaO/stackoverflown/tree/master/questions/note-tray-21753641
+#include <QtWidgets>
+#include <list>
 
 // Note.h
 
@@ -20,71 +16,85 @@ class Note : public QWidget
 {
    Q_OBJECT
 public:
-    Note(TrayMenu *trayMenu, QWidget *parent = 0);
-    ~Note();
+   Note(TrayMenu *trayMenu, QWidget *parent = {});
 private:
-    QScopedPointer<Ui::Note> m_ui;
-    QPointer<TrayMenu> m_traymenu;
+   Ui::Note m_ui;
+   QPointer<TrayMenu> m_traymenu;
 };
 
 // TrayMenu.h
 
 class Note;
-class TrayMenu : public QSystemTrayIcon
-{
+class TrayMenu : public QObject {
+   Q_OBJECT
 public:
-    TrayMenu();
-    ~TrayMenu();
-    void createMainContextMenu();
-    void newNote();
+   TrayMenu();
+   void createMainContextMenu();
+   void newNote();
 private:
-    QSystemTrayIcon mainIcon;
-    QMenu mainContextMenu;
-    QList<QSharedPointer<Note> > noteList;
+   QSystemTrayIcon m_mainIcon;
+   QMenu m_mainContextMenu;
+   std::list<Note> m_notes;
 };
 
 // TrayMenu.cpp
 
-TrayMenu::TrayMenu(){
-    mainIcon.setIcon(QIcon(QPixmap("C:\\program.png")));
-    mainIcon.setVisible(true);
-    mainIcon.show();
-
-    createMainContextMenu();
+template <int N> auto decode64(const char (&arg)[N], int rows) {
+   auto const raw = QByteArray::fromBase64(QByteArray::fromRawData(arg, N-1));
+   QImage img((const quint8 *)raw.data(), rows, rows, raw.size()/rows, QImage::Format_MonoLSB);
+   img = std::move(img).convertToFormat(QImage::Format_Indexed8);
+   img.setColor(1, qRgba(0, 0, 0, 0)); // make transparent
+   return img;
 }
 
-TrayMenu::~TrayMenu() {}
+// convert baseline_language_black_18dp.png -flatten -negate -monochrome mono:-|base64 -b80
+static const char language_d64[] =
+      "/////w//////D/////8P/z/A/w//DwD+D/8BAPwP/wEA+A9/IEbgDz8cjuEPPxyPww8fHg+HDw+PHw8P"
+      "DwAAAA8PAAAADw8AAAAPx8c/Pg7Hzz8+DsfHHz4Ox4c/Pg7Hxz8/DsfHPz4ODwAAAA4PAAAADw8AAAAP"
+      "H48fjw8fHg+HDz8cj4MPPxiH4Q9/IMbgD/8AAPAP/wMA/A//DwD/D/8/4P8P/////w//////D/////8P";
+static const auto language_icon = decode64(language_d64, 36);
+
+// convert baseline_note_add_black_18dp.png -flatten -negate -monochrome mono:-|base64 -b80
+static const char note_add_d64[] =
+      "/////w//////D/////8PfwDA/w8/AMD/Dz8AAP8PPwAY/w8/ADD8Dz8AePwPPwDw8A8/APjhDz8A8OMP"
+      "PwDwxw8/AJDCDz8AAMAPPwAAwA8/AATADz8AD8APPwAGwA8/AA/ADz8ABsAPP/D/wA8/8P/ADz/w/8AP"
+      "PwAOwA8/AAfADz8ADsAPPwAGwA8/AAbADz8AAMAPPwAAwA8/AADAD38AAOAP/////w//////D/////8P";
+static const auto note_add_icon = decode64(note_add_d64, 36);
+
+TrayMenu::TrayMenu() {
+   m_mainIcon.setIcon(QPixmap::fromImage(language_icon));
+   m_mainIcon.setVisible(true);
+   m_mainIcon.show();
+   createMainContextMenu();
+}
 
 void TrayMenu::newNote() {
-    QSharedPointer<Note> note(new Note(this));
-    noteList << note;
+   m_notes.emplace_back(this);
+   m_notes.back().show();
 }
 
-void TrayMenu::createMainContextMenu(){
-    QAction *actionNewNote = mainContextMenu.addAction("Neue Notiz");
-    mainContextMenu.addSeparator();
-    QAction *actionExitProgram = mainContextMenu.addAction("Programm beenden");
+void TrayMenu::createMainContextMenu() {
+   auto *actionNewNote = m_mainContextMenu.addAction("Neue Notiz");
+   m_mainContextMenu.addSeparator();
+   auto *actionExitProgram = m_mainContextMenu.addAction("Programm beenden");
 
-    actionNewNote->setIcon(QIcon("C:\\new.ico"));
-    actionNewNote->setIconVisibleInMenu(true);
+   actionNewNote->setIcon(QPixmap::fromImage(note_add_icon));
+   actionNewNote->setIconVisibleInMenu(true);
 
-    QObject::connect(actionNewNote, &QAction::triggered, this, &TrayMenu::newNote);
-    QObject::connect(actionExitProgram, &QAction::triggered, qApp, &QCoreApplication::quit);
+   QObject::connect(actionNewNote, &QAction::triggered, this, &TrayMenu::newNote);
+   QObject::connect(actionExitProgram, &QAction::triggered, QCoreApplication::quit);
 
-    mainIcon.setContextMenu(&mainContextMenu);
+   m_mainIcon.setContextMenu(&m_mainContextMenu);
 }
 
 // Note.cpp
 
 Note::Note(TrayMenu *trayMenu, QWidget *parent) :
-    QWidget(parent),
-    m_ui(new Ui::Note),
-    m_traymenu(trayMenu)
+   QWidget(parent),
+   m_traymenu(trayMenu)
 {
-    m_ui->setupUi(this);
+   m_ui.setupUi(this);
 }
-
-Note::~Note() {}
 
 // main.cpp
 
@@ -93,5 +103,4 @@ int main(int argc, char ** argv) {
    TrayMenu menu;
    return app.exec();
 }
-
 #include "main.moc"
--- questions/note-tray-21753641/note-tray-21753641.pro
+++ questions/note-tray-21753641/note-tray-21753641.pro
@@ -1,9 +1,7 @@
-QT       += core gui
-greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
-
-TARGET = note-tray-21753641
-
-TEMPLATE = app
-
-
+CONFIG += c++14
+QT = widgets
+DEFINES += \
+  QT_DEPRECATED_WARNINGS \
+  QT_DISABLE_DEPRECATED_BEFORE=0x060000 \
+  QT_RESTRICTED_CAST_FROM_ASCII
 SOURCES += main.cpp

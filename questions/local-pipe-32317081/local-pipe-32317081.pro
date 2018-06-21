243ec6119c7f673e806b04b6f4e48ebdea72d709
Kuba Ober <kuba@mareimbrium.org>
2018-06-20 20:04:27 -0400

    Have AppPipe support Qt 4 and Qt 5. Add write buffering support. Note: Qt 4 support still crashes.

--- questions/local-pipe-32317081/local-pipe-32317081.pro
+++ questions/local-pipe-32317081/local-pipe-32317081.pro
@@ -1,6 +1,12 @@
 # local-pipe-32317081.pro
-QT = core-private
-TARGET = local-pipe-32317081
-CONFIG += c++11
+QT = core
+greaterThan(QT_MAJOR_VERSION, 4): QT = core-private testlib
+else: CONFIG += qtestlib
+DEFINES += \
+  QT_DEPRECATED_WARNINGS \
+  QT_DISABLE_DEPRECATED_BEFORE=0x060000 \
+  QT_RESTRICTED_CAST_FROM_ASCII
+CONFIG += console c++14
+CONFIG -= app_bundle
 TEMPLATE = app
 SOURCES = main.cpp
--- questions/local-pipe-32317081/main.cpp
+++ questions/local-pipe-32317081/main.cpp
@@ -1,66 +1,156 @@
 // https://github.com/KubaO/stackoverflown/tree/master/questions/local-pipe-32317081
-#include <QtCore>
+// This project is compatible with Qt 4 and Qt 5
+#include <QtTest>
+#include <private/qiodevice_p.h>
 #include <private/qringbuffer_p.h>
+#include <algorithm>
+#include <climits>
+
+#ifndef Q_DECL_OVERRIDE
+#define Q_DECL_OVERRIDE
+#endif
+
+class AppPipePrivate : public QIODevicePrivate {
+public:
+#if QT_VERSION < QT_VERSION_CHECK(5,7,0)
+   QRingBuffer buffer;
+   QRingBuffer writeBuffer;
+   int writeBufferChunkSize;
+#endif
+   const QByteArray *writeData;
+   AppPipePrivate() : writeData(0) { writeBufferChunkSize = 4096; }
+};
 
 /// A simple point-to-point intra-process pipe. The other endpoint can live in any
 /// thread.
 class AppPipe : public QIODevice {
    Q_OBJECT
-   QRingBuffer m_buf;
-   Q_SLOT void _a_write(const QByteArray & data) {
-      if (! openMode() & QIODevice::ReadOnly) return; // We must be readable.
-      m_buf.append(data);
+   Q_DECLARE_PRIVATE(AppPipe)
+   Q_SLOT void _a_write(const QByteArray &data) {
+      Q_D(AppPipe);
+      if (!(d->openMode & QIODevice::ReadOnly)) return; // We must be readable.
+      d->buffer.append(data); // This is a chunk shipped from the source.
       emit hasIncoming(data);
       emit readyRead();
    }
-public:
-   AppPipe(AppPipe * other, QIODevice::OpenMode mode, QObject * parent = {}) :
-      AppPipe(other, parent) {
-      open(mode);
-   }
-   AppPipe(AppPipe * other = {}, QObject * parent = {}) : QIODevice(parent) {
-      addOther(other);
-   }
-   void addOther(AppPipe * other) {
-      if (other) {
-          connect(this, &AppPipe::hasOutgoing, other, &AppPipe::_a_write, Qt::UniqueConnection);
-          connect(other, &AppPipe::hasOutgoing, this, &AppPipe::_a_write, Qt::UniqueConnection);
+   void hasOutgoingLong(const char *data, qint64 len) {
+      while (len) {
+         qint64 const size = std::min(qint64(INT_MAX), len);
+         emit hasOutgoing(QByteArray(data, size));
+         data += size;
+         len -= size;
       }
    }
-   void removeOther(AppPipe * other) {
-      disconnect(this, &AppPipe::hasOutgoing, other, &AppPipe::_a_write);
-      disconnect(other, &AppPipe::hasOutgoing, this, &AppPipe::_a_write);
+public:
+   AppPipe(QIODevice::OpenMode mode, QObject *parent = 0) :
+      QIODevice(*new AppPipePrivate, parent) {
+      open(mode);
    }
-   void close() override {
+   AppPipe(AppPipe *other, QIODevice::OpenMode mode, QObject *parent = 0) :
+      QIODevice(*new AppPipePrivate, parent) {
+      open(mode);
+      addOther(other);
+   }
+   AppPipe(AppPipe *other, QObject *parent = 0) :
+      QIODevice(*new AppPipePrivate, parent) {
+      addOther(other);
+   }
+   AppPipe(QObject *parent = 0) :
+      QIODevice(*new AppPipePrivate, parent) {
+   }
+   ~AppPipe() Q_DECL_OVERRIDE {}
+   void addOther(AppPipe *other) {
+      if (other) {
+         connect(this, SIGNAL(hasOutgoing(QByteArray)), other, SLOT(_a_write(QByteArray)), Qt::UniqueConnection);
+         connect(other, SIGNAL(hasOutgoing(QByteArray)), this, SLOT(_a_write(QByteArray)), Qt::UniqueConnection);
+      }
+   }
+   void removeOther(AppPipe *other) {
+      disconnect(this, SIGNAL(hasOutgoing(QByteArray)), other, SLOT(_a_write(QByteArray)));
+      disconnect(other, SIGNAL(hasOutgoing(QByteArray)), this, SLOT(_a_write(QByteArray)));
+   }
+   void flush() {
+      Q_D(AppPipe);
+      while (!d->writeBuffer.isEmpty()) {
+         QByteArray const data = d->writeBuffer.read();
+         emit hasOutgoing(data);
+         emit bytesWritten(data.size());
+      }
+   }
+   void close() Q_DECL_OVERRIDE {
+      Q_D(AppPipe);
+      flush();
       QIODevice::close();
-      m_buf.clear();
+      d->buffer.clear();
    }
-   qint64 writeData(const char * data, qint64 maxSize) override {
-      if (maxSize > 0)
-         hasOutgoing(QByteArray(data, maxSize));
-      return maxSize;
+   qint64 write(const QByteArray &data) { // This is an optional optimization. The base method works OK.
+      Q_D(AppPipe);
+      QScopedValueRollback<const QByteArray*> back(d->writeData);
+      if (!(d->openMode & Text))
+         d->writeData = &data;
+      return QIODevice::write(data);
    }
-   qint64 readData(char * data, qint64 maxLength) override {
-      return m_buf.read(data, maxLength);
+   qint64 writeData(const char *data, qint64 len) Q_DECL_OVERRIDE {
+      Q_D(AppPipe);
+      bool buffered = !(d->openMode & Unbuffered);
+      if (buffered && (d->writeBuffer.size() + len) > d->writeBufferChunkSize)
+         flush();
+      if (!buffered
+          || len > d->writeBufferChunkSize
+          || (len == d->writeBufferChunkSize && d->writeBuffer.isEmpty()))
+      {
+         if (d->writeData && d->writeData->data() == data && d->writeData->size() == len)
+            emit hasOutgoing(*d->writeData);
+         else
+            hasOutgoingLong(data, len);
+      }
+      else
+         memcpy(d->writeBuffer.reserve(len), data, len);
+      return len;
    }
-   qint64 bytesAvailable() const override {
-      return m_buf.size() + QIODevice::bytesAvailable();
+   qint64 readData(char *data, qint64 len) Q_DECL_OVERRIDE {
+#if QT_VERSION < QT_VERSION_CHECK(5,7,0)
+      Q_D(AppPipe);
+      qint64 hadRead = 0;
+      while (len && !d->buffer.isEmpty()) {
+         qint64 nextSize = d->buffer.nextDataBlockSize();
+         int size = d->buffer.read(data, std::min(len, nextSize));
+         hadRead += size;
+         data += size;
+         len -= size;
+      }
+      return hadRead;
+#else
+      Q_UNUSED(data); Q_UNUSED(len);
+      return 0; // all the data is in the read buffer already
+#endif
    }
-   bool canReadLine() const override {
-      return QIODevice::canReadLine() || m_buf.canReadLine();
+#if QT_VERSION < QT_VERSION_CHECK(5,7,0)
+   bool canReadLine() const Q_DECL_OVERRIDE {
+      Q_D(const AppPipe);
+      return d->buffer.indexOf('\n') != -1 || QIODevice::canReadLine();
    }
-   bool isSequential() const override { return true; }
+#endif
+   bool isSequential() const Q_DECL_OVERRIDE { return true; }
    Q_SIGNAL void hasOutgoing(const QByteArray &);
    Q_SIGNAL void hasIncoming(const QByteArray &);
 };
 
-int main(/*…*/)
-{
-   /*…*/
-   AppPipe end1 { nullptr, QIODevice::ReadWrite };
-   AppPipe end2 { &end1, QIODevice::ReadWrite };
-   // the pipes are open ready to use
-   /*…*/
-}
+class TestAppPipe : public QObject {
+   Q_OBJECT
+   Q_SLOT void basic() {
+      AppPipe end1(QIODevice::ReadWrite);
+      AppPipe end2(&end1, QIODevice::ReadWrite);
+      QVERIFY(end1.isOpen() && end1.isWritable() && end1.isReadable());
+      QVERIFY(end2.isOpen() && end2.isWritable() && end2.isReadable());
+      static const char hello[] = "Hello There!";
+      end1.write(hello);
+      end1.flush();
+      QCOMPARE(end2.readAll(), QByteArray(hello));
+   }
+
+};
+
+QTEST_MAIN(TestAppPipe)
 
 #include "main.moc"

427ce6e97593c06c8591eaf3748ee1971a1b4348
Kuba Ober <kuba@mareimbrium.org>
2018-07-25 22:00:12 -0400

    WIP - some generic code can be reused for other projects

--- questions/qgraphicsitem-stream-51492181/main.cpp
+++ questions/qgraphicsitem-stream-51492181/main.cpp
@@ -1,42 +1,80 @@
 #include <QtWidgets>
 #include <QtTest>
 #include <type_traits>
+#include <utility>
+
+#if 0
+/* Common polymorphic type machinery */
 
 namespace detail {
-template <typename T> struct MetatypeFunctionHelperImpl {
+
+template <typename T> struct CopyableFunctionHelperImpl {
    static void Destruct(void *t) { static_cast<T*>(t)->~T(); }
    static void *Construct(void *where, const void *t) {
-      if (t)
-         return new (where) T(*static_cast<const T*>(t));
-      return new (where) T;
+      return t ? new (where) T(*static_cast<const T*>(t)) : new (where) T;
    }
    static void Save(QDataStream &ds, const void *t) { ds << *static_cast<const T*>(t); }
    static void Load(QDataStream &stream, void *t) { ds >> *static_cast<T*>(t); }
 };
-template <typename B, typename T> struct BaseMetatypeFunctionhelper<B, T> :
-      MetatypeFunctionHelperImpl<T> {};
+
+template <typename T> struct NonCopyableFunctionHelperImpl {
+   static void Destruct(void *t) { static_cast<T*>(t)->~T(); }
+   static void *Construct(void *where, const void *t) { return (!t) ? new (where) T : nullptr; }
+   static void Save(QDataStream &ds, const void *t) { ds << *static_cast<const T*>(t); }
+   static void Load(QDataStream &stream, void *t) { ds >> *static_cast<T*>(t); }
+};
+
+template <typename B, bool Enabled = false> struct PolymorphicTraits;
+
+template <typename B> struct PolymorphicTraits<B, true> {
+   // By default, discriminate types by their metatype Id
+   using discriminator_type = int;
+   template <typename T> static discriminator_type type() { return qMetaTypeId<T>(); }
+   template <typename T> static void *create(void *copy) { return QMetaType::create(type<T>(), copy); }
+   template <typename T> static discriminator_type registerDiscriminator() { return type<T>(); }
+};
+
 }
 
-#define DECLARE_CUSTOM_METATYPE(BASE_TYPE, TYPE) DECLARE_CUSTOM_METATYPE_IMPL(BASE_TYPE, TYPE)
+#define DECLARE_POLYMORPHIC_METATYPE(BASE_TYPE, TYPE) DECLARE_POLYMORPHIC_METATYPE_IMPL(BASE_TYPE, TYPE)
+#define DECLARE_POLYMORPHIC_METATYPE_IMPL(BASE_TYPE, TYPE)           \
+   QT_BEGIN_NAMESPACE                                                \
+   template <> struct QtMetaTypePrivate::QMetaTypeFunctionHelper     \
+   <typename std::enable_if<std::is_copy_constructible<TYPE>::value, TYPE>::type, true> : \
+   detail::CopyableFunctionHelperImpl<TYPE> {};                   \
+   template <> struct QtMetaTypePrivate::QMetaTypeFunctionHelper     \
+   <typename std::enable_if<!std::is_copy_constructible<TYPE>::value, TYPE>::type, true> : \
+   detail::NonCopyableFunctionHelperImpl<TYPE> {};                \
+   template <>                                                       \
+   int qRegisterMetaType(const char *typeName, TYPE *dummy,          \
+   typename QtPrivate::MetaTypeDefinedHelper<T, QMetaTypeId2<T>::Defined && !QMetaTypeId2<T>::IsBuiltIn>::DefinedType defined) \
+{ \
+   QT_PREPEND_NAMESPACE(QByteArray) normalizedTypeName = QMetaObject::normalizedType(typeName); \
+   detail::PolymorphicTraits<BASE_TYPE>::registerDiscriminator<TYPE>();   \
+   return qRegisterNormalizedMetaType<TYPE>(normalizedTypeName, dummy, defined); \
+   } \
+   template <> struct QMetaTypeId<TYPE> {                            \                                                                  \
+   enum { Defined = 1 };                                          \
+   static int qt_metatype_id() {                                  \
+   static QBasicAtomicInt metatype_id = Q_BASIC_ATOMIC_INITIALIZER(0); \
+   if (const int id = metatype_id.loadAcquire())               \
+   return id;                                                  \
+   const int newId = qRegisterMetaType<TYPE>(#TYPE,            \
+   reinterpret_cast<TYPE*>(quintptr(-1)));                  \
+   metatype_id.storeRelease(newId);                            \
+   return newId;                                               \
+   }                                                              \
+   };                                                                \
+   QT_END_NAMESPACE
 
-#define DECLARE_CUSTOM_METATYPE_IMPL(BASE_TYPE, TYPE) \
-   QT_BEGIN_NAMESPACE \
-   template <> \
-   struct QtMetaTypePrivate::QMetaTypeFunctionHelper<TYPE, true> : \
-   BaseMetatypeFunctionhelper<BASE_TYPE, TYPE> {}; \
 
+
+#define DEFINE_POLYMORPHIC_METATYPE(TYPE) DEFINE_POLYMORPHIC_METATYPE_IMPL(TYPE)
+#define DEFINE_POLYMORPHIC_METATYPE_IMPL(TYPE) \
+   static
 
 Q_GLOBAL_STATIC(QReadWriteLock, aa)
 
-                template <typename T> struct QGraphicsItemHelper {
-                   static void Destruct(void *t) { static_cast<T*>(t)->~T(); }
-                   static void *Construct(void *where, const void *t) {
-                      Q_ASSERT(!t);
-                      return new (where) T;
-                   }
-                   static void Save(QDataStream &ds, const void *t) { ds << *static_cast<const T*>(t); }
-                   static void Load(QDataStream &ds, void *t) { ds >> *static_cast<T*>(t); }
-                };
 
 template <typename T> bool checkTypeEnum() {
    auto const &en = QMetaEnum::fromType<T>();
@@ -67,26 +105,82 @@ template <typename T> int enumToTypeId(int enumVal) {
    struct QMetaTypeId<TYPE>                                             \
 {                                                                    \
    enum { Defined = 1 };                                            \
-   static int qt_metatype_id()                                      \
-{                                                            \
-   static QBasicAtomicInt metatype_id = Q_BASIC_ATOMIC_INITIALIZER(0); \
-   if (const int id = metatype_id.loadAcquire())            \
-   return id;                                           \
-   const int newId = qRegisterMetaType< TYPE >(#TYPE,       \
-   reinterpret_cast< TYPE *>(quintptr(-1)));  \
-   metatype_id.storeRelease(newId);                         \
-   return newId;                                            \
-   }                                                            \
-   };                                                                   \
-   QT_END_NAMESPACE
+
 
 DECLARE_GRAPHICS_METAITEM(QGraphicsEllipseItem)
+#endif
+
+
+template <typename T, typename B> struct NonCopyableFunctionHelper {
+   using base_type = B;
+   static void Destruct(void *t) { static_cast<T*>(t)->~T(); }
+   static void *Construct(void *where, const void *t) { return (!t) ? new (where) T : nullptr; }
+   static void Save(QDataStream &ds, const void *t) { ds << *static_cast<const T*>(t); }
+   static void Load(QDataStream &ds, void *t) { ds >> *static_cast<T*>(t); }
+};
+
+#define DECLARE_POLYMORPHIC_METATYPE(BASE_TYPE, TYPE) DECLARE_POLYMORPHIC_METATYPE_IMPL(BASE_TYPE, TYPE)
+#define DECLARE_POLYMORPHIC_METATYPE_IMPL(BASE_TYPE, TYPE)                          \
+   QT_BEGIN_NAMESPACE                                                               \
+   template <> struct QtMetaTypePrivate::QMetaTypeFunctionHelper                    \
+   <typename std::enable_if<!std::is_copy_constructible<TYPE>::value, TYPE>::type, true> : \
+      detail::NonCopyableFunctionHelperImpl<TYPE, BASE_TYPE> {};                    \
+   QT_END_NAMESPACE                                                                 \
+   Q_DECLARE_METATYPE_IMPL(TYPE)
+
+//DECLARE_POLYMORPHIC_METATYPE(QGraphicsItem, QGraphicsEllipseItem)
+
+
+/*Interface*/
+template <> struct QtMetaTypePrivate::QMetaTypeFunctionHelper
+      <typename std::enable_if<!std::is_copy_constructible<QGraphicsEllipseItem>::value, QGraphicsEllipseItem>::type, true> :
+      NonCopyableFunctionHelper<QGraphicsEllipseItem, QGraphicsItem> {};
+Q_DECLARE_METATYPE(QGraphicsEllipseItem)
+
+template <typename T, typename Enable = std::false_type> struct BaseTraits;
+
+template <> struct BaseTraits<QGraphicsItem, std::true_type> {
+   using base_type = QGraphicsItem;
+   struct pair_type { int itemType; int typeId; };
+   template <typename T>
+   static typename std::enable_if<std::is_base_of<base_type, T>::value>::type registerType() {
+      qRegisterMetaTypeStreamOperators<T>();
+      registerMapping(qMetaTypeId<T>(), T::type());
+   }
+   static pair_type registerMapping(int typeId, int d);
+};
+
+QDataStream &operator<<(QDataStream &, QGraphicsItem *g);
+QDataStream &operator>>(QDataStream &, QGraphicsItem *&g);
+
+/*Implementation*/
+
+QDataStream &operator<<(QDataStream &ds, const QGraphicsItem *g) {
+   auto mapping = BaseTraits<QGraphicsItem, std::true_type>::registerMapping(QMetaType::UnknownType, g->type());
+   QMetaType::save(ds, mapping.typeId, g);
+   return ds;
+}
+
+QDataStream &operator>>(QDataStream &, QGraphicsItem *&g) {
+   int type = 0;
+   auto read = ds.device()->peek(reinterpret_cast<char*>(&type), sizeof(type));
+   if (read != sizeof(type)) {
+      ds.setStatus(QDataStream::ReadPastEnd);
+      return ds;
+   }
+   auto mapping = BaseTraits<QGraphicsItem, std::true_type>::registerMapping(type, 0);
+   const QMetaType mt(mapping.typeId);
+   g = mt.create();
+   if (g) mt.load()
+   QMetaClassIn
+}
+
 
 class ItemStream {
    Q_GADGET
    QDataStream &ds;
    QGraphicsItem &item;
-   public:
+public:
    enum Type {
       QGraphicsEllipseItem = QGraphicsEllipseItem::Type,
       QGraphicsPathItem = QGraphicsPathItem::Type,

b26d4a43b6d0deb8b93345d2ad6cc4f95be3e970
Kuba Ober <kuba@mareimbrium.org>
2018-11-05 11:50:29 -0500

    Modernize.

--- questions/label-font-18896933/label-font-18896933.pro
+++ questions/label-font-18896933/label-font-18896933.pro
@@ -1,17 +1,8 @@
-#-------------------------------------------------
-#
-# Project created by QtCreator 2013-09-19T10:24:09
-#
-#-------------------------------------------------
-
-QT       += core gui
-
-greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
-
-TARGET = label-font-18896933
-TEMPLATE = app
-
-
-SOURCES += main.cpp
-
-HEADERS  +=
+QT = gui
+greaterThan(QT_MAJOR_VERSION, 4): QT = widgets
+DEFINES += \
+  QT_DEPRECATED_WARNINGS \
+  QT_DISABLE_DEPRECATED_BEFORE=0x060000 \
+  QT_RESTRICTED_CAST_FROM_ASCII
+CONFIG += c++14
+SOURCES = main.cpp
--- questions/label-font-18896933/main.cpp
+++ questions/label-font-18896933/main.cpp
@@ -1,38 +1,37 @@
-#include <QApplication>
-#include <QWidget>
-#include <QLabel>
-#include <QString>
-#include <QVBoxLayout>
-#include <QDebug>
+// https://github.com/KubaO/stackoverflown/tree/master/questions/label-font-18896933
+// This project is compatible with Qt 4 and Qt 5
+#include <QtGui>
+#if QT_VERSION >= QT_VERSION_CHECK(5, 0, 0)
+#include <QtWidgets>
+#endif
 
-static bool isFixedPitch(const QFont & font) {
-    const QFontInfo fi(font);
-    qDebug() << fi.family() << fi.fixedPitch();
-    return fi.fixedPitch();
+bool isFixedPitch(const QFont &font) {
+   const QFontInfo fi(font);
+   qDebug() << fi.family() << fi.fixedPitch();
+   return fi.fixedPitch();
 }
 
-static QFont getMonospaceFont(){
-    QFont font("monospace");
-    if (isFixedPitch(font)) return font;
-    font.setStyleHint(QFont::Monospace);
-    if (isFixedPitch(font)) return font;
-    font.setStyleHint(QFont::TypeWriter);
-    if (isFixedPitch(font)) return font;
-    font.setFamily("courier");
-    if (isFixedPitch(font)) return font;
-    return font;
+QFont getMonospaceFont() {
+   QFont font("monospace");
+   if (isFixedPitch(font)) return font;
+   font.setStyleHint(QFont::Monospace);
+   if (isFixedPitch(font)) return font;
+   font.setStyleHint(QFont::TypeWriter);
+   if (isFixedPitch(font)) return font;
+   font.setFamily("courier");
+   if (isFixedPitch(font)) return font;
+   return font;
 }
 
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
-    QString text("0123456789ABCDEF");
-    QWidget w;
-    QVBoxLayout * l = new QVBoxLayout(&w);
-    QLabel * l1 = new QLabel(text);
-    l1->setFont(getMonospaceFont());
-    l->addWidget(l1);
-    l->addWidget(new QLabel(text));
-    w.show();
-    return a.exec();
+int main(int argc, char *argv[]) {
+   QApplication a(argc, argv);
+   QString text("0123456789ABCDEF");
+   QWidget w;
+   QVBoxLayout layout(&w);
+   QLabel label1(text), label2(text);
+   label1.setFont(getMonospaceFont());
+   layout.addWidget(&label1);
+   layout.addWidget(&label2);
+   w.show();
+   return a.exec();
 }

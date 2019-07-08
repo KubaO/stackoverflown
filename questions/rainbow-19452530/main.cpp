663abf3b7ba9dfa6e7fb7f55c9b95d12371d21bd
Kuba Ober <kuba@bertec.com>
2019-07-08 12:20:14 -0400

    Modernize.

--- questions/rainbow-19452530/main.cpp
+++ questions/rainbow-19452530/main.cpp
@@ -1,77 +1,76 @@
-#include <QApplication>
-#include <QPainter>
-#include <QPixmap>
-#include <QLabel>
-#include <algorithm>
+// https://github.com/KubaO/stackoverflown/tree/master/questions/rainbow-19452530
+#include <QtGui>
+#if QT_VERSION_MAJOR > 4
+#include <QtWidgets>
+#endif
+#include <array>
 #include <cmath>
 
-QColor wavelengthToColor(qreal lambda)
-{
-    // Based on: http://www.efg2.com/Lab/ScienceAndEngineering/Spectra.htm
-    // The foregoing is based on: http://www.midnightkite.com/color.html
-    struct Color {
-    public:
-        qreal c[3];
-        QColor toColor(qreal factor) const {
-            qreal const gamma = 0.8;
-            int ci[3];
-            for (int i = 0; i < 3; ++i) {
-                ci[i] = c[i] == 0.0 ? 0.0 : qRound(255 * pow(c[i] * factor, gamma));
-            }
-            return QColor(ci[0], ci[1], ci[2]);
-        }
-    } color;
-    qreal factor = 0.0;
+QColor wavelengthToColor(qreal lambda) {
+   // Based on: http://www.efg2.com/Lab/ScienceAndEngineering/Spectra.htm
+   // The foregoing is based on: http://www.midnightkite.com/color.html
+   struct Color {
+      qreal red, green, blue;
+      QColor toColor(qreal factor) const {
+         auto const map = [factor](qreal c) -> qreal {
+            constexpr qreal gamma = 0.8;
+            return pow(c * factor, gamma);
+         };
+         return QColor::fromRgbF(map(red), map(green), map(blue));
+      }
+   } color{};
 
-    static qreal thresholds[] = { 380, 440, 490, 510, 580, 645, 780 };
-    for (unsigned int i = 0; i < sizeof(thresholds)/sizeof(thresholds[0]); ++ i) {
-        using std::swap;
-        qreal t1 = thresholds[i], t2 = thresholds[i+1];
-        if (lambda < t1 || lambda >= t2) continue;
-        if (i%2) swap(t1, t2);
-        color.c[i % 3] = (i < 5) ? (lambda - t2) / (t1-t2) : 0.0;;
-        color.c[2-i/2] = 1.0;
-        factor = 1.0;
-        break;
-    }
+   struct Threshold {
+      qreal l1, l2;
+      Color (*color)(qreal);
+      qreal (*factor)(qreal) = nullptr;
+   };
+   static const std::array<Threshold, 8> thresholds{
+      Threshold{380, 420, nullptr, [](qreal x){ return 0.3 + 0.7*x; }},
+      Threshold{380, 440, [](qreal x){ return Color{1-x, 0, 1}; }},
+      Threshold{440, 490, [](qreal x){ return Color{0, x, 1}; }},
+      Threshold{490, 510, [](qreal x){ return Color{0, 1, 1-x}; }},
+      Threshold{510, 580, [](qreal x){ return Color{x, 1, 0}; }},
+      Threshold{580, 645, [](qreal x){ return Color{1, 1-x, 0}; }},
+      Threshold{645, 780, [](qreal  ){ return Color{1, 0, 0}; }},
+      Threshold{700, 780, nullptr, [](qreal x){ return 1 - 0.7*x; }}
+   };
 
-    // Let the intensity fall off near the vision limits
-    if (lambda >= 380 && lambda < 420) {
-        factor = 0.3 + 0.7*(lambda-380) / (420 - 380);
-    }
-    else if (lambda >= 700 && lambda < 780) {
-        factor = 0.3 + 0.7*(780 - lambda) / (780 - 700);
-    }
-    return color.toColor(factor);
+   qreal factor = 1.0;
+   for (auto &thr : thresholds) {
+      if (lambda < thr.l1 || lambda >= thr.l2) continue;
+      auto x = (thr.l1 - lambda) / (thr.l1 - thr.l2);
+      if (thr.factor) factor = thr.factor(x);
+      // To let the intensity fall off near the vision limits
+      if (thr.color) color = thr.color(x);
+   }
+
+   return color.toColor(factor);
 }
 
-QPixmap rainbow(int w, int h)
-{
-    QPixmap pm(w, h);
-    QPainter p(&pm);
-    qreal f1 = 1.0/400;
-    qreal f2 = 1.0/780;
-    for (int x = 0; x < w; ++ x) {
-        // Iterate across frequencies, not wavelengths
-        qreal lambda = 1.0/(f1-(x/qreal(w)*(f1-f2)));
-        p.setPen(wavelengthToColor(lambda));
-        p.drawLine(x, 0, x, h);
-    }
-    return pm;
+QPixmap rainbow(int w, int h) {
+   QPixmap pixmap(w, h);
+   QPainter p(&pixmap);
+   qreal f1 = 1.0 / 400;
+   qreal f2 = 1.0 / 780;
+   for (int x = 0; x < w; ++x) {
+      // Iterate across frequencies, not wavelengths
+      qreal lambda = 1.0 / (f1 - (x / qreal(w) * (f1 - f2)));
+      p.setPen(wavelengthToColor(lambda));
+      p.drawLine(x, 0, x, h);
+   }
+   return pixmap;
 }
 
 class RainbowLabel : public QLabel {
-protected:
-    void resizeEvent(QResizeEvent *) {
-        setPixmap(rainbow(width(), height()));
-    }
+  protected:
+   void resizeEvent(QResizeEvent *) override { setPixmap(rainbow(width(), height())); }
 };
 
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
-    RainbowLabel l;
-    l.resize(600, 100);
-    l.show();
-    return a.exec();
+int main(int argc, char *argv[]) {
+   QApplication a(argc, argv);
+   RainbowLabel l;
+   l.resize(600, 100);
+   l.show();
+   return a.exec();
 }
